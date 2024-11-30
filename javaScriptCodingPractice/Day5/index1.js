09eeadf7712f998dbfa2e87399ff002445dbc4b2
Basit Ali <opoali911@gmail.com>
2024-11-29 16:23:20 -0800

    changing assignment

--- javaScriptCodingPractice/Day5/index1.js
+++ javaScriptCodingPractice/Day5/index1.js
@@ -5,3 +5,8 @@ let a=3;
 let b=4;
 let c=3-(a=b+1); 
 document.write('<br> the value of c is: ',c)
+
+
+let d,e,f;
+d=e=f=12;
+document.write("<br> the value of d is: ",d,"<br> the value of e is: ",e,"<br> the value of f is: ",f)
