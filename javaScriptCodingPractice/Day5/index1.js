e87dcff534e9b1042f885ad8f8699766de25a3aa
Basit Ali <opoali911@gmail.com>
2024-11-29 16:41:53 -0800

    increment and decrement operators are used to increment or decrement the value of a variable by 1. The ++ operator is used to increment the value of a variable by 1, while the -- operator is used to decrement the value of a variable by 1.

--- javaScriptCodingPractice/Day5/index1.js
+++ javaScriptCodingPractice/Day5/index1.js
@@ -10,3 +10,20 @@ document.write('<br> the value of c is: ',c)
 let d,e,f;
 d=e=f=12;
 document.write("<br> the value of d is: ",d,"<br> the value of e is: ",e,"<br> the value of f is: ",f)
+
+
+let n=3;
+n=n+4;
+n+=10;
+document.write("<br> the value of n is: ",n)
+
+
+let m=5;
+m*=2;
+document.write("<br> the value of m is: ",m)
+
+let p=10;
+document.write("<br> the value of p is: ",++p)
+document.write("<br> the value of p is: ",p++)
+document.write("<br> the value of p is: ",p)
+
