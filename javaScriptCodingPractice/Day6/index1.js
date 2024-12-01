5625f6c3b18d2517c4ee31e3e427f83418b63aac
Basit Ali <opoali911@gmail.com>
2024-11-30 16:26:16 -0800

    Start the string comparison in javaScript

--- javaScriptCodingPractice/Day6/index1.js
+++ javaScriptCodingPractice/Day6/index1.js
@@ -1,4 +1,5 @@
 // comparison operators
+// i-Number comparison
 let num1=10;
 let num2=5;
 document.write("10 is greater than 5:",num1>num2);
@@ -10,3 +11,9 @@ let num4=10;
 document.write("<br> 10 is equal to 10: ", num3==num4)
 document.write("<br> 10 is greater then and equal to 10: ",num3>=num4)
 document.write("<br> 10 is less than and equal to 10: ",num3<=num4)
+document.write("<br> 10 is not equal to 10: ",num3!=num4)
+
+// ii-String Comparison 
+let str1='A';
+let str2='Z';
+document.write("<br> A is equal to Z: ",str1==str2)
