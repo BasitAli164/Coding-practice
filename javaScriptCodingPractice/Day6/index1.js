bec7a11e0700749548d863d3d8e23f054a84ca3e
Basit Ali <opoali911@gmail.com>
2024-11-30 16:19:27 -0800

    practice on  greater,less , and equal comparison operator

--- javaScriptCodingPractice/Day6/index1.js
+++ javaScriptCodingPractice/Day6/index1.js
@@ -1,4 +1,12 @@
 // comparison operators
 let num1=10;
 let num2=5;
-document.write("num1 is greater than num2:",num1>num2);
+document.write("10 is greater than 5:",num1>num2);
+document.write("<br> 10 is less than or equal to 5 :",num1<num2);
+
+
+let num3=10;
+let num4=10;
+document.write("<br> 10 is equal to 10: ", num3==num4)
+document.write("<br> 10 is greater then and equal to 10: ",num3>=num4)
+document.write("<br> 10 is less than and equal to 10: ",num3<=num4)
