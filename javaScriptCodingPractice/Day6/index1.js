c6781df141687ef0f2bfcadeb0c98dda5a47fdf2
Basit Ali <opoali911@gmail.com>
2024-11-30 16:41:37 -0800

    start comparison with different data type

--- javaScriptCodingPractice/Day6/index1.js
+++ javaScriptCodingPractice/Day6/index1.js
@@ -17,3 +17,23 @@ document.write("<br> 10 is not equal to 10: ",num3!=num4)
 let str1='A';
 let str2='Z';
 document.write("<br> A is equal to Z: ",str1==str2)
+
+let str3='apple';
+let str4='banana';
+document.write("<br> apple is greater than banana :",str3>str4);
+document.write("<br> apple is less than or equal to banana :",str3<str4);
+document.write("<br> apple is equal to banana: ",str3==str4)
+
+
+//comparsion with different type
+let diff1='1';
+let diff2=1
+document.write("<br> '1' is equal to 1: ",diff1==diff2)
+document.write("<br> '1' is greater than 1 is: ",diff1>diff2)
+document.write("<br> '1' is less than 1 is: ",diff1<diff2)
+
+let diff3='01';
+let diff4=1;
+document.write("<br> '01' is equal to the 1 is :",diff3==diff4)
+document.write("<br> '01' is greater than to 1 is: " ,diff3>diff4)
+document.write("<br> '01' is less than to 1 is: " ,diff3<diff4)
