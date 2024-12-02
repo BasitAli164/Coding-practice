bbc9b4a61f58c351e8aac6d4da8788c37680b77c
Basit Ali <opoali911@gmail.com>
2024-12-01 23:32:27 -0800

    finaaly i commplete the multiple if else block statement of javaScript

--- javaScriptCodingPractice/Day7/index2.js
+++ javaScriptCodingPractice/Day7/index2.js
@@ -7,5 +7,13 @@ document.write(subjectMarks>30?"<br>You are pass.Congratulation !":"<br>You are 
 // multiple if else blocks  
 let percentage=prompt("Enter your Percentage: ",70);
 if(percentage>90 && percentage<=100){
-    document.write("<br>")
+    document.write("<br>Your Grade is: A")
+}else if(percentage>=80 && percentage<=89){
+    document.write("<br>Your Grade is: B")
+}else if(percentage>=70 && percentage<=79){
+    document.write("<br>Your Grade is: C")
+}else if(percentage>=60 && percentage<=69){
+    document.write("<br> Your Grade is: D")
+}else{
+    document.write("<br>Your Grade is: E")
 }
