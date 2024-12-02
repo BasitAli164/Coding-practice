51f327cf824bedc50eeac8a2604dc2590590b5df
Basit Ali <opoali911@gmail.com>
2024-12-01 17:36:18 -0800

    finished the if statement in conditional block

--- javaScriptCodingPractice/Day7/index1.js
+++ javaScriptCodingPractice/Day7/index1.js
@@ -2,3 +2,20 @@ let year=2005;
 if(year>2004){
     document.write("Your are eligible to vote")
 }
+
+let age=prompt("Enter your exact age: ");
+if(age>=18){
+    document.write("<br>Your are eligible to vote");
+}
+
+if(0){
+    document.write("<br>This will not be printed");
+}
+if(1){
+    document.write("<br>This will be printed")
+    console.log("This will be printed in console ")
+}
+// Eles cluase:
+if(age>=18){
+    document.write("<br>You are right person for this work ")
+}
