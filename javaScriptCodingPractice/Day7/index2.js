9e2ce6b2139a342d3cff2e8249b453204a08b6ed
Basit Ali <opoali911@gmail.com>
2024-12-01 23:21:25 -0800

    practice perfectly the ternary operator

---
+++ javaScriptCodingPractice/Day7/index2.js
@@ -0,0 +1,5 @@
+let studentAge=20;
+document.write(studentAge>20?"<br>you are a good person":"<br>You are try to show good person")
+
+let subjectMarks=prompt("Enter you subject marks: ");
+document.write(subjectMarks>30?"<br>You are pass.Congratulation !":"<br>You are fail.Try Again")
