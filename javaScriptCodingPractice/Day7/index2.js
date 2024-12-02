fd1f95a6dcd150665c8c280a1d4871e4294edcae
Basit Ali <opoali911@gmail.com>
2024-12-01 23:27:56 -0800

    State the multiple if else block code

--- javaScriptCodingPractice/Day7/index2.js
+++ javaScriptCodingPractice/Day7/index2.js
@@ -3,3 +3,9 @@ document.write(studentAge>20?"<br>you are a good person":"<br>You are try to sho
 
 let subjectMarks=prompt("Enter you subject marks: ");
 document.write(subjectMarks>30?"<br>You are pass.Congratulation !":"<br>You are fail.Try Again")
+
+// multiple if else blocks  
+let percentage=prompt("Enter your Percentage: ",70);
+if(percentage>90 && percentage<=100){
+    document.write("<br>")
+}
