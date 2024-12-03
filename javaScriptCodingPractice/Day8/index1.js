17e9022f022bb6455cbffc1b4d31b169abeb3552
Basit Ali <opoali911@gmail.com>
2024-12-02 17:59:46 -0800

    so some code are write of logical operator

--- javaScriptCodingPractice/Day8/index1.js
+++ javaScriptCodingPractice/Day8/index1.js
@@ -13,3 +13,13 @@ document.write("<br> false or false: ",false||false)
 //programs 
 let a=10,b=40;
 console.log(a||b)
+
+let hours=7;
+if(hours<10 || hours>18){
+    document.write("<br>The office is closed ........")
+}
+ let isWeakend=true
+ if(hours<5 || hours>10||isWeakend){
+    document.write("<br>The office is closed confirmly.....")
+ }
+ 
