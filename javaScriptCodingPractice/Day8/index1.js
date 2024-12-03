6db2c2f78fdf6d5481ae03bdb11302ad554d7449
Basit Ali <opoali911@gmail.com>
2024-12-02 17:53:42 -0800

    Again start the logical operator or

--- javaScriptCodingPractice/Day8/index1.js
+++ javaScriptCodingPractice/Day8/index1.js
@@ -1,1 +1,15 @@
-// logical operator 
+// logical operator
+
+// Or Operator
+
+
+// the rule of or operator is :
+document.write("The rule and regulation of Or operator:")
+document.write("<br>true or false: ",true||false);
+document.write("<br>ture or true: ",true||true);
+document.write("<br>false or true: ",false||true);
+document.write("<br> false or false: ",false||false)
+
+//programs 
+let a=10,b=40;
+console.log(a||b)
