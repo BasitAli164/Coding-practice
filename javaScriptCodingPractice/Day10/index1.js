939ee81bfd50473fb28ca156ac2db3ad7f5c1ffa
Basit Ali <opoali911@gmail.com>
2024-12-04 15:22:53 -0800

    another code in loop

--- javaScriptCodingPractice/Day10/index1.js
+++ javaScriptCodingPractice/Day10/index1.js
@@ -1,41 +1,55 @@
-document.write("hello world")
-let i=1;
-while(i<=2){
-    alert(i);
-    i++
-}
-let j=1;
-while(j<=8){
-    document.write("<br>Loop is itrating ......");
-    j++;
-}
-let k=1;
-while(k<=10){
-    document.write("<br>The value of i is : ",k)
-    k++
-}
-let a=10
-while(a>=1){
-    document.write("<br>The value of a is : ",a);
-    a--
-}
-let b=10;
-do{
-    document.write("<br>The value of b is : ",b);
-    b++
-}while(b<=20);
-let sum=0
-for(let y=0;y<=10;y++){
-   
-    sum+=y;
-}
-document.write("<br>total number is: ",sum)
-
-let reuslt=0; let x=1;
-
-do{
-    reuslt+=x;
-    x++;
+    document.write("hello world")
+    let i=1;
+    while(i<=2){
+        alert(i);
+        i++
+    }
+    let j=1;
+    while(j<=8){
+        document.write("<br>Loop is itrating ......");
+        j++;
+    }
+    let k=1;
+    while(k<=10){
+        document.write("<br>The value of i is : ",k)
+        k++
+    }
+    let a=10
+    while(a>=1){
+        document.write("<br>The value of a is : ",a);
+        a--
+    }
+    let b=10;
+    do{
+        document.write("<br>The value of b is : ",b);
+        b++
+    }while(b<=20);
+    let sum=0
+    for(let y=0;y<=10;y++){
     
-}while(x<=100)
-    document.write(" <br>the result is : ",reuslt)
+        sum+=y;
+    }
+    document.write("<br>total number is: ",sum)
+
+    let reuslt=0; let x=1;
+
+    do{
+        reuslt+=x;
+        x++;
+        
+    }while(x<=100)
+        document.write(" <br>the result is : ",reuslt);
+
+
+    let addition = 0;
+
+    while (true) {
+    
+      let value = +prompt("Enter a number", '');
+    
+      if (!value) break; // (*)
+    
+      addition += value;
+    
+    }
+    alert( 'Sum: ' + addition );
