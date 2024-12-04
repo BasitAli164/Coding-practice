d9790590730d764fe276a7e0b051f06614bc2088
Basit Ali <opoali911@gmail.com>
2024-12-04 14:20:52 -0800

    start while loop

--- javaScriptCodingPractice/Day10/index1.js
+++ javaScriptCodingPractice/Day10/index1.js
@@ -1,6 +1,16 @@
 document.write("hello world")
 let i=1;
-while(i<=5){
+while(i<=2){
     alert(i);
     i++
+}
+let j=1;
+while(j<=8){
+    document.write("<br>Loop is itrating ......");
+    j++;
+}
+let k=1;
+while(k<=10){
+    document.write("<br>The value of i is : ",i)
+    k++
 }
