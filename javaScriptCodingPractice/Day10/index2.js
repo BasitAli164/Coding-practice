07a2cfa1153bf9b81de3472c436dbc69e47d0c52
Basit Ali <opoali911@gmail.com>
2024-12-04 15:47:36 -0800

    write code in nesting loop

---
+++ javaScriptCodingPractice/Day10/index2.js
@@ -0,0 +1,14 @@
+for(let i=1;i<3;i++){
+    for(let j=1;j<3;j++){
+        document.write("<br>The value of i is:- ",i,"  and j is:- ",j)
+    }
+}
+
+outer:
+for(let k=0;k<15;k++){
+    console.log("hello....",k)
+    for(let m=0;m<10;m++){
+        if(m>=5)break
+        console.log("inner.....")
+    }
+}
