5f9384e2dd5ce93fb9b14744171c8118a7175e62
Basit Ali <opoali911@gmail.com>
2024-12-03 19:23:28 -0800

    Almost finished the task

--- javaScriptCodingPractice/Day9/index2.js
+++ javaScriptCodingPractice/Day9/index2.js
@@ -7,4 +7,17 @@ document.write("<br>",alert(1)||2||alert(3))
 document.write("<br>",1&&null&&2)
 //T-4
 alert(alert(1)&&2&&alert(3))
-document.write("<br>",alert(1)&&alert(3))
+document.write("<br>",alert(1)&&alert(3));
+
+document.write("<br>",null || 2 && 3||4);
+//T-5
+let age=prompt("Enter your age: ",0);
+if(age>14&&age<90){
+    document.write("Inclusively........")
+}
+if(!(age>14&&age<90)){
+    document.write("Inclusively 2........")
+}
+if (-1 || 0) alert( 'first' );
+if (-1 && 0) alert( 'second' );
+if (null || -1 && 1) alert( 'third' );
