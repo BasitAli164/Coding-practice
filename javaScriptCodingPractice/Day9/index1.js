cbd43158ecbd7d21082bd67d76b9994eb0e2d09e
Basit Ali <opoali911@gmail.com>
2024-12-03 18:19:20 -0800

    start code with logical and operator

--- javaScriptCodingPractice/Day9/index1.js
+++ javaScriptCodingPractice/Day9/index1.js
@@ -1,7 +1,17 @@
 // and operator in logical operator
 //Rules of and operator
 document.write("Rules of and operator:::");
-document.write("<br> true and true",true&&true)
-document.write("<br> true and false",true&&false)
-document.write("<br> false and false",false&&false)
-document.write("<br> false and true",false&&true)
+document.write("<br> true and true: ",true&&true)
+document.write("<br> true and false: ",true&&false)
+document.write("<br> false and false: ",false&&false)
+document.write("<br> false and true: ",false&&true);
+
+
+let hour=12;
+let miniute=30;
+if(hour==12 && miniute==30){
+    document.write("<br>Time is 12:30");
+}
+if(hour==12&&miniute==23){
+    document.write("<br>Time is 12:23");
+}
