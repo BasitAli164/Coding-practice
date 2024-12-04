38ad3954412eeb7028e1b847c472902bd57a5eb1
Basit Ali <opoali911@gmail.com>
2024-12-03 19:32:31 -0800

    make a simple login task in javaScript

--- javaScriptCodingPractice/Day9/index3.js
+++ javaScriptCodingPractice/Day9/index3.js
@@ -1,1 +1,17 @@
 // mini project (login project)
+let userName=prompt("Who is there:  ", "");
+if(userName=="admin"){
+    let password=prompt("Password ?");
+    if(password=="12345"){
+        alert("Welcome Admin");
+    }else if(password=="" || password==null){
+        alert("Wrong password")
+    }else{
+        alert("Cancel")
+    }
+}
+else if(userName==""||userName==null){
+    alert("I don't know you")
+}else{
+    alert("Access denied")
+}
