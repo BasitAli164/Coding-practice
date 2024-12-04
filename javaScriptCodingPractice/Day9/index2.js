9b1267588f1ef9b2462904c3bd5fd399c1827115
Basit Ali <opoali911@gmail.com>
2024-12-03 19:04:59 -0800

    working on task

--- javaScriptCodingPractice/Day9/index2.js
+++ javaScriptCodingPractice/Day9/index2.js
@@ -1,2 +1,10 @@
 //Task:
-document.write(null||2||undefined)
+//T-1
+document.write(null||2||undefined);
+//T-2
+document.write("<br>",alert(1)||2||alert(3))
+//T-3
+document.write("<br>",1&&null&&2)
+//T-4
+alert(alert(1)&&2&&alert(3))
+document.write("<br>",alert(1)&&alert(3))
