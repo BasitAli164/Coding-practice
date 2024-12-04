a3dbf4c6d73eaefb1f4bc97d44f11022299a4d3d
Basit Ali <opoali911@gmail.com>
2024-12-03 18:15:06 -0800

    identify the rule and regulation of and operator

--- javaScriptCodingPractice/Day9/index1.js
+++ javaScriptCodingPractice/Day9/index1.js
@@ -1,1 +1,7 @@
 // and operator in logical operator
+//Rules of and operator
+document.write("Rules of and operator:::");
+document.write("<br> true and true",true&&true)
+document.write("<br> true and false",true&&false)
+document.write("<br> false and false",false&&false)
+document.write("<br> false and true",false&&true)
