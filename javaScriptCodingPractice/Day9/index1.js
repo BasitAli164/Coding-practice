b933b998c1c6518292daf8c7902446fcfdb533ff
Basit Ali <opoali911@gmail.com>
2024-12-03 18:50:52 -0800

    some code of line wirte for not logical operator

--- javaScriptCodingPractice/Day9/index1.js
+++ javaScriptCodingPractice/Day9/index1.js
@@ -19,11 +19,19 @@ if(hour==12&&miniute==23){
 if(1&&0){
     document.write("<br>1 and 0 is true");
 }
-alert( 1 && 0 ); // 0
-alert( 1 && 5 ); // 5
-alert( null && 5 ); // null
-alert( 0 && "no matter what" ); // 0
-alert( 1 && 2 && null && 3 );
-lert( 1 && 2 && 3 );
+// alert( 1 && 0 ); // 0
+// alert( 1 && 5 ); // 5
+// alert( null && 5 ); // null
+// alert( 0 && "no matter what" ); // 0
+// alert( 1 && 2 && null && 3 );
+// lert( 1 && 2 && 3 );
 
 // Not oeprator
+document.write("<br><br>Rules of Not operator:::");
+document.write("<br>!true: ",!true)
+document.write("<br>!false: ",!false)
+
+
+// code
+document.write("<br>!0 is: ",!0);
+document.write("<br> !!'Non' is: ",!!'Non');
