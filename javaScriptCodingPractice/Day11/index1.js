29741de599ad757c1740c8784e9bd7f95bc3e01b
Basit Ali <opoali911@gmail.com>
2024-12-05 16:54:47 -0800

    Now start the task in swtich statement in javaScript

--- javaScriptCodingPractice/Day11/index1.js
+++ javaScriptCodingPractice/Day11/index1.js
@@ -44,3 +44,21 @@ switch(name){
     default:
         document.write("Wrong name!")
 }
+// another code
+let arg = prompt("Enter a value?");
+switch (arg) {
+  case '0':
+  case '1':
+    alert( 'One or zero' );
+    break;
+
+  case '2':
+    alert( 'Two' );
+    break;
+
+  case 3:
+    alert( 'Never executes!' );
+    break;
+  default:
+    alert( 'An unknown value' );
+}
