29b6de4f01b7ad9fd096399a2b08306d21d8f3a7
Basit Ali <opoali911@gmail.com>
2024-12-05 16:37:15 -0800

    start code for switch statement

---
+++ javaScriptCodingPractice/Day11/index1.js
@@ -0,0 +1,13 @@
+// Switch Case:
+let a=2+2;
+switch(a){
+    case 0: document.write("Too Samll....")
+    break;
+    case 2: document.write("near to value....")
+    break;
+    case 4: document.write("Excellent: the value is match.....")
+    break;
+    case 10: document.write("Too large......")
+    break;
+    default:document.write("Invalid Number.")
+}
