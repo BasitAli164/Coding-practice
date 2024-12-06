ad3afc756d2347d4ec970033ca8a6796807eb2e3
Basit Ali <opoali911@gmail.com>
2024-12-05 16:41:45 -0800

    now doing coding in switch statement

--- javaScriptCodingPractice/Day11/index1.js
+++ javaScriptCodingPractice/Day11/index1.js
@@ -11,3 +11,23 @@ switch(a){
     break;
     default:document.write("Invalid Number.")
 }
+let b=2;
+switch(b){
+    case 0: document.write(" <br>Too Samll....")
+    case 2: document.write(" <br>near to value....")
+    case 4: document.write(" <br>Excellent: the value is match.....")
+    case 10: document.write(" <br>Too large......")
+    default:document.write(" <br>Invalid Number.")
+}
+
+let a1 = "1";
+let b1 = 0;
+
+switch (+a1) {
+  case b1 + 1:
+    alert("this runs, because +a is 1, exactly equals b+1");
+    break;
+
+  default:
+    alert("this doesn't run");
+}
