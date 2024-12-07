727c2f265b50e963859f9349e0dca9611026d51e
Basit Ali <opoali911@gmail.com>
2024-12-07 14:54:31 -0800

    we can the alternative default parameter as if statement , or operater ,and nullish coalescing operator

--- javaScriptCodingPractice/Day13/index1.js
+++ javaScriptCodingPractice/Day13/index1.js
@@ -9,3 +9,16 @@ add(3,0)
 add(4+2,45);
 
 
+function displayMessage(text){
+    text=text||"Don't give anything";
+    document.write(`The message is: ${text}<br>`)
+}
+displayMessage();
+displayMessage("Hello World");
+
+function displayAge(age){
+    document.write(age??"You don't have an age <br>") //  ?? is called nullish coalescing operator. It returns the first non-null value.
+}
+displayAge();
+displayAge(23);
+displayAge(null);
