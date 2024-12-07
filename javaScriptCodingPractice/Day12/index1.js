b034e6c0a1c2bbc27c57954d3ec056beb4cf7fb8
Basit Ali <opoali911@gmail.com>
2024-12-06 22:44:25 -0800

    introduc the local variable in function

--- javaScriptCodingPractice/Day12/index1.js
+++ javaScriptCodingPractice/Day12/index1.js
@@ -1,5 +1,11 @@
 // function 
-function showMessage(){
+function showMessage1(){
     alert("My name is Basit Ali...")
 }
-showMessage();
+showMessage1();
+
+function showMessage2(){
+    let message="Hellow World!"; // local variable
+    alert(message);
+}
+showMessage2()
