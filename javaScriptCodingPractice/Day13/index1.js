33faef4e8a810535490b3f778d45d7fdd0803930
Basit Ali <opoali911@gmail.com>
2024-12-07 15:18:35 -0800

    make a mini project of basic arithmetic operation to understand the return function or statment

--- javaScriptCodingPractice/Day13/index1.js
+++ javaScriptCodingPractice/Day13/index1.js
@@ -22,3 +22,60 @@ function displayAge(age){
 displayAge();
 displayAge(23);
 displayAge(null);
+
+// retrun 
+function sum(a,b){
+    return a+b;
+}
+console.log("The sum of 2 and 3 is:",sum(2,3)); // 5
+
+//mini project on reuturn function
+function basicArithmetic(a,b,operator){
+    if(operator==="+"){
+        return a+b;
+    }
+    else if(operator==="-"){
+        if(a>b){
+            return a-b;
+        }else{
+            return b-a;
+        }
+    }
+    else if(operator==="*"){
+        return a*b;
+    }else if(operator==="/"){
+        if(a>b){
+            return a/b;
+        }else{
+            return b/a;
+        }
+    }
+    else if(operator==="%"){
+        if(a>b){
+            return a%b;
+        }else{
+            return b%a;
+        }
+    }
+    else if(operator=="^"){
+        if(a>b){
+            return Math.pow(a,b)
+        }else{
+            return Math.pow(b,a);
+        }
+
+    }
+    else{
+        return "Invalid operator"
+    }
+}
+document.write("<br>The sum of 5 and 4 is: ",basicArithmetic(5,4,"+"))
+document.write("<br>The subtarction of 5 and 4 is: ",basicArithmetic(5,4,"-"))
+document.write("<br>The subtarction of 5 and 14 is: ",basicArithmetic(5,14,"-"))
+document.write("<br>The multiplication of 5 and 4 is: ",basicArithmetic(5,4,"*"))
+document.write("<br>The division of 5 and 4 is: ",basicArithmetic(5,4,"/"));
+document.write("<br>The division of 5 and 14 is: ",basicArithmetic(5,14,"/"));
+document.write("<br>The modulus of 5 and 4 is: ",basicArithmetic(5,4,"%"));
+document.write("<br>The modulus of 5 and 14 is: ",basicArithmetic(5,14,"%"));
+document.write("<br>The power of 5 and 4 is: ",basicArithmetic(5,4,"^"));
+document.write("<br>The power of 5 and 14 is: ",basicArithmetic(5,14,"^"));
