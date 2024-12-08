6137dcaade42282b84af0c92b8372f9c03485f95
Basit Ali <opoali911@gmail.com>
2024-12-07 18:30:32 -0800

    last commit in function expression

--- javaScriptCodingPractice/Day13/index2.js
+++ javaScriptCodingPractice/Day13/index2.js
@@ -1,6 +1,20 @@
 // function Expression 
+// Syntax of function expression is let variableName=function(){};
 let sumFunction=function(){
     return 5+1;
 };
-document.write("The function is: ",sumFunction) // it hold the instance of the above function means copy of the function 
-document.write("The function value after execution: ",sumFunction())
+document.write("<br>The function is: ",sumFunction) // it hold the instance of the above function means copy of the function 
+document.write("<br>The function value after execution: ",sumFunction());
+
+let funcExpression=function(){
+    return "<br>The function expression like this: let func=function(){};";
+}
+// let copyFunc=funcExpression(); this is wrong 
+let copyFunc=funcExpression;
+
+
+
+// document.write(copyFunc());
+document.write("<br>",copyFunc());
+
+document.write(funcExpression())
