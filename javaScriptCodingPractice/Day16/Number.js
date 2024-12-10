122b29fd807628c488312337bd9301f1710a031a
Basit Ali <opoali911@gmail.com>
2024-12-10 09:14:22 -0800

    Start the Number Data type

---
+++ javaScriptCodingPractice/Day16/Number.js
@@ -0,0 +1,6 @@
+let billion=1000000000;
+let billion2=1_000_000_000; // both are same ,this achive through syntax sugar
+console.log("Billion 1: ",billion," and Billion 2:",billion2)
+
+let billion3=1e9;
+console.log(`Billion 3: ${billion3}`)
