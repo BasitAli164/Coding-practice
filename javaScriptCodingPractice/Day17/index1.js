114139b01c396eb5c798633550e5e034e6aba989
Basit Ali <opoali911@gmail.com>
2024-12-11 09:49:02 -0800

    start the method in String

---
+++ javaScriptCodingPractice/Day17/index1.js
@@ -0,0 +1,11 @@
+// String Data Type
+let singleQuote='Single Quotes';
+let doubleQuote="Double Quotes";
+let backticks=`Backticks, ${singleQuote} and ${doubleQuote} used to express string in JavaScript`;
+document.write(backticks);
+
+let guestlist=`Guest: 
+Basit
+Muhammad`
+alert(`${guestlist}`)
+document.write(`<br>Length of Guest is: ${guestlist.length}`)
