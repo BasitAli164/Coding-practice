1e8a40f838ef895f7a5adc50565fe5b08f337bd3
Basit Ali <opoali911@gmail.com>
2024-12-11 10:18:22 -0800

    explore the position or access method and Changing the Case methods

--- javaScriptCodingPractice/Day17/index1.js
+++ javaScriptCodingPractice/Day17/index1.js
@@ -9,3 +9,18 @@ Basit
 Muhammad`
 alert(`${guestlist}`)
 document.write(`<br>Length of Guest is: ${guestlist.length}`)
+document.write(`<br>Length of Guest is: ${singleQuote.length}`)
+//String methods
+//i-at()
+console.log("Access single Character from single quotes",singleQuote[2])
+console.log("Access single Character from single quotes",singleQuote.at(2))
+console.log("Access single Character from single quotes",singleQuote.at(-1))
+console.log("Access single Character from single quotes",singleQuote.at(-5))
+console.log("Access single Character from single quotes",singleQuote[-5])
+
+//for .. of loop
+for(let char of singleQuote){
+    console.log("Charactier in SingleQuote: ",char)
+}
+
+console.log(`The single quote in upper Case is: ${singleQuote.toUpperCase()} and lower Case is: ${singleQuote.toLowerCase()}`)
