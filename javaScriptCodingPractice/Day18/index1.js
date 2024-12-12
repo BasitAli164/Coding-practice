ebf62c89e0e032d2181bc5025225883ee1e6ecf4
Basit Ali <opoali911@gmail.com>
2024-12-12 09:03:04 -0800

    start coding with array data type

---
+++ javaScriptCodingPractice/Day18/index1.js
@@ -0,0 +1,12 @@
+// Array
+let arr=[1,2,3,4,5,6]
+alert(arr) // it amazing becuause it show the premitive data type (number ,string,boolean,array) all data inside the array
+
+let fruit=['apple','banana','orange'];
+alert(fruit)
+
+let  mixArray=["Apple",{name:"Basit"},true,50,function(){alert("Hello!")},[3,4,5]]
+alert(mixArray)
+
+let speacial=[null,undefined]
+alert(speacial) // does not show the null and undefined
