935c9348106f23b26cdad11b7d5e2eaed6a4f637
Basit Ali <opoali911@gmail.com>
2024-12-14 10:56:13 -0800

    make an advance level program

---
+++ javaScriptCodingPractice/Day20/index2.js
@@ -0,0 +1,6 @@
+let number=[1,2,3,4,5,6];
+function getElement(arr,index){
+    console.log("The particular element is:",arr[index])
+}
+getElement(number,0)
+// in this case the time complexity is O(1)
