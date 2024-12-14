3986d12861cbce4cf54ebbc51394adf575640ff6
Basit Ali <opoali911@gmail.com>
2024-12-13 20:33:25 -0800

    the layout of the login and signUp form ready

--- LearningReact/ReactPractice/src/components/LoginForm/Login.jsx
+++ LearningReact/ReactPractice/src/components/LoginForm/Login.jsx
@@ -2,33 +2,62 @@ import React from 'react'
 import './Login.css'
 
 const Login = () => {
-  return (
-    <>
-    <div className="container">
-        <div className="header">
-            <h1>Login Form</h1>
-        </div>
-        <div className="underline"></div>
-        <div className="form">
-            <form >
-                <label htmlFor="Username:">Username:</label>
-                <input
-                 type="text" 
-                 name="" 
-                 id=""
-                 placeholder="Enter Your Username"
-                 minLength={5}
-                 maxLength={100}
-                 title="Username must be more than 5 character"
-                 
-                  />
-            </form>
-        </div>
-        <div className="button"></div>
-        
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="container">
+                <div className="header">
+                    <h1>Login form</h1>
+                </div>
+                <div className="underline"></div>
+                <div className="form">
+                    <form >
+                        <div className='formdiv'>
+                            <label htmlFor="Username:">Username:</label>
+                            <input
+                                type="text"
+                                name="Username"
+                                id="Username"
+                                placeholder="Enter your username"
+                                minLength={5}
+                                maxLength={100}
+                                title="Username must be more than 5 character"
+
+                            />
+                        </div>
+                        <div className='formdiv'>
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="Enter your email"
+                                title="Email should be in proper email format"
+                            />
+                        </div>
+                        <div className='formdiv'>
+                            <label htmlFor="password">Password:</label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="Enter Your Password"
+                                min={5}
+                                max={30}
+                                title="Enter a strong password"
+                            />
+                        </div>
+                        <div className="button">
+                            <button className='isSubmitted'>Login</button>
+                            <button className='isSubmitted'>SignUp</button>
+                        </div>
+
+
+                    </form>
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default Login
