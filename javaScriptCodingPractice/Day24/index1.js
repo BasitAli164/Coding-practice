bdc87d927c6abc77a15437fd094dc6d33ef49481
Basit Ali <opoali911@gmail.com>
2024-12-18 10:27:19 -0800

    explore more thing in rest parameter

--- javaScriptCodingPractice/Day24/index1.js
+++ javaScriptCodingPractice/Day24/index1.js
@@ -22,3 +22,14 @@ const add2=(...num)=>{// rest parameter
    })
 }
 console.log("Addition :",add2(1,2,3,4,5,56));
+
+const buyProduct=(...price)=>{
+   
+    let reduceMethod=price.reduce((acc,curr)=>{
+        return acc+curr
+    },0)
+    return reduceMethod;
+}
+console.log("Buy 20 pen and price of one pen is 20 :",buyProduct(20,22,24,25,206,260,210))
+
+ 
