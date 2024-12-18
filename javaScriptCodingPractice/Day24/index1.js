429efa6e23dc615634a8e9f1c337a8458119008b
Basit Ali <opoali911@gmail.com>
2024-12-18 09:29:09 -0800

    coding with arguments key words

--- javaScriptCodingPractice/Day24/index1.js
+++ javaScriptCodingPractice/Day24/index1.js
@@ -1,4 +1,16 @@
 function add(a,b){
     console.log(`Value of a is ${a} and b is: ${b}`);
+    console.log("Argument keyword: ",arguments);
+    return a+b
 }
-add(1,2)
+console.log(`Sum :${add(1,2,3,4,5,7)}`);
+
+const multiply=function(){
+    let product=1;
+    for(let i=0;i<arguments.length;i++){
+        product*=arguments[i];
+
+    }
+    return product;
+}
+console.log("Multiply: ",multiply(1,2,3));
