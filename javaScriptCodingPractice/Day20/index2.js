bedfd94e70112cadde6932726a6562dd3d687cd7
Basit Ali <opoali911@gmail.com>
2024-12-21 10:17:01 -0800

    Explore concat method in array

--- javaScriptCodingPractice/Day20/index2.js
+++ javaScriptCodingPractice/Day20/index2.js
@@ -1,6 +1,10 @@
-let number=[1,2,3,4,5,6];
-function getElement(arr,index){
-    console.log("The particular element is:",arr[index])
-}
-getElement(number,0)
-// in this case the time complexity is O(1)
+// Array methods
+let array=[1,2,3,4,5];
+let arr2=["basit","Muhammad","sakina","khatija",'kubra'];
+let arr3=arr2.concat(array)
+console.log(arr3) 
+console.log(`new trick:`,arr3.concat([true,false],"basit",3,5))
+
+let arrary2=array.concat(["basit",true],[3,4,5],{age:23,status:true},"king ali",5,false)
+console.log(`array2 is:`,arrary2)
+
