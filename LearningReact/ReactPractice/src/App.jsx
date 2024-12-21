635217f0901f018621f8ac6233427bf021563627
Basit Ali <opoali911@gmail.com>
2024-12-20 21:57:40 -0800

    explore memo and useMemo again

--- LearningReact/ReactPractice/src/App.jsx
+++ LearningReact/ReactPractice/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Form from './components/Form/Form'
+import UseMemo from './components/ReactHook/UseMemo'
 
 const App = () => {
+  const [count,setCount]=useState(0)
+  console.log("parent")
   return (
     <>
-     <Form/> 
+    <UseMemo/>
+    <div>
+      <h3>Count: {count}</h3>
+      <button onClick={()=>setCount(count+10)}>Parent</button>
+    </div>
     </>
   )
 }
---
+++ LearningReact/ReactPractice/src/components/ReactHook/UseRef.jsx
@@ -0,0 +1,12 @@
+import React, { useRef } from 'react'
+
+const UseRef = () => {
+    const inputRef=useRef();
+  return (
+    <div>
+      <input type="text" name="" id="" />
+    </div>
+  )
+}
+
+export default UseRef
