140fc03b356dd50b1d5009154027eaac31fc8c80
Basit Ali <opoali911@gmail.com>
2024-12-22 11:08:50 -0800

    explore the filter method of an array with diffrent senario

--- javaScriptCodingPractice/Day28/index.js
+++ javaScriptCodingPractice/Day28/index.js
@@ -20,9 +20,43 @@ let users=[
         id:5,
         userName:"kubra"
     },
+    {
+        id:6,
+        userName:"Basit"
+    }
 ];
 let serachingUser=users.find((item)=>{
     return item.id>1;
 });
 console.log(`User is: ${serachingUser}`)
 console.log("User is: ",serachingUser)
+
+let serachfindIndex=users.findIndex((item)=>{
+    return item.userName==="Sakina"&& item.id===3
+})
+let serachfindIndex1=users.findIndex((item)=>{
+    return item.id===9
+})
+let serachfindIndex2=users.findIndex((item)=>{
+    return item.id===1
+})
+
+console.log("Index of Sakina is: ",serachfindIndex)// return 2
+console.log("Index of id 9 is: ",serachfindIndex1) // -1 because not found
+console.log("Index of id 1 is: ",serachfindIndex2) // return 0
+
+let throughFilter=users.filter((item)=>{
+    return item.id==1;
+})
+
+let throughFilter1=users.filter((item)=>{
+    return item.id>1
+})
+let throughFilter2=users.filter((item)=>{
+    return item.id>10
+})
+console.log("Through filter id equal to 1 is:",throughFilter);
+console.log("Through filter id > 1 is:",throughFilter1);
+console.log("Through filter id > 10 is:",throughFilter2);// retrun [] because there no element which are id>10
+
+
