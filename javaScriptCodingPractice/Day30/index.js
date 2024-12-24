e84191b1ecc5b662182f68c24c8be236ef144aa7
Basit Ali <opoali911@gmail.com>
2024-12-24 09:23:56 -0800

    start codeing on reduce method of array

--- javaScriptCodingPractice/Day30/index.js
+++ javaScriptCodingPractice/Day30/index.js
@@ -1,1 +1,5 @@
 //Array Methods
+// Reduce 
+let reduceMethod=[1,2,3,4,5,6,7,8];
+let returnValue=reduceMethod.reduce((acc,curr)=>acc+curr,[12]);
+console.log("Apply the reduce method:",returnValue)
