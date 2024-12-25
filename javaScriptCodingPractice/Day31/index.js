b78af61c3c0c67aca38012c53b6e9cc6bf4b3f28
Basit Ali <opoali911@gmail.com>
2024-12-25 10:32:54 -0800

    task 6 complete

--- javaScriptCodingPractice/Day31/index.js
+++ javaScriptCodingPractice/Day31/index.js
@@ -1,11 +1,15 @@
 // Array Task
 //Task-1
 function camelize(str){
-    let changeStr= str.split('-').map((word,index)=>index==0?word:word[0].toUpperCase()+word.slice(1)).join('');
-    console.log("Change Str is:",changeStr);
+   return str
+          .split('-')
+          .map((word,index)=>{
+            return index==0?word:word[0].toUpperCase()+word.slice(1)
+          })
+          .join('');
 }
-camelize("background-color");
-camelize("list-style-image")
+console.log(camelize("background-color"))
+console.log(camelize("list-style-image"))
 
 //Task-2
 
@@ -42,3 +46,14 @@ let sortedFun=function(arr){
 let sortingPurpose = ["HTML", "JavaScript", "CSS"];
 console.log("Sorted Array is: ",sortedFun(sortingPurpose));
 
+//Task-6
+let user=[
+    {name:"basit",age:23},
+    {name:"ALI",age:23},
+    {name:"kiyan",age:23},
+]
+
+let convert=user.map((item)=>{
+    return item.name.split('').join('')
+})
+console.log("Coverted array is: ",convert)
