4182f4b9c889228b780ba5b2845aeffc95490485
Basit Ali <opoali911@gmail.com>
2024-12-25 22:38:07 -0800

    explore the memo ,useMemo ,useRef and useForwardRef ,controlled and uncontrolled componenet

--- LearningReact/ReactPractice/src/App.jsx
+++ LearningReact/ReactPractice/src/App.jsx
@@ -1,18 +1,11 @@
 import React, { useState } from 'react'
 import './App.css'
-import Form from './components/Form/Form'
-import UseMemo from './components/ReactHook/UseMemo'
+import UseRef from './components/ReactHook/UseRef'
 
 const App = () => {
-  const [count,setCount]=useState(0)
-  console.log("parent")
   return (
     <>
-    <UseMemo/>
-    <div>
-      <h3>Count: {count}</h3>
-      <button onClick={()=>setCount(count+10)}>Parent</button>
-    </div>
+    <UseRef/>
     </>
   )
 }
--- LearningReact/ReactPractice/src/components/ReactHook/UseRef.jsx
+++ LearningReact/ReactPractice/src/components/ReactHook/UseRef.jsx
@@ -1,11 +1,32 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const UseRef = () => {
-    const inputRef=useRef();
+    const inputRef=useRef(null);
+    const [word ,setWord]=useState('');
+    const handleNext=(e)=>{
+      console.log("e is:",e)
+      console.log("e target",e.target)
+      console.log("e target",e.target.value)
+      setWord(e.target.value)
+    }
+    
+    
+    const hnadleButton=()=>{
+      const initalValue=inputRef.current.value;
+      console.log("Inital  value is: ", initalValue)
+      console.log("current is :",inputRef.current)
+      console.log("value is :",inputRef.current.value)
+      
+    }
+
   return (
     <div>
-      <input type="text" name="" id="" />
-    </div>
+      <h1>useRef Hook in React</h1>
+      <input type="text" ref={inputRef}  />
+      <input type="text" name="" id="" placeholder='next' value={word} onChange={handleNext} />
+      <button onClick={hnadleButton}>Get InputValue </button>
+      i
+   </div>
   )
 }
 
