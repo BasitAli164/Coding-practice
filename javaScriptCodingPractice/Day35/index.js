60baf02608cf2db98c422e6d254a4ca022548926
Basit Ali <opoali911@gmail.com>
2024-12-29 13:54:47 +0500

    explore the starting date also

--- javaScriptCodingPractice/Day35/index.js
+++ javaScriptCodingPractice/Day35/index.js
@@ -1,3 +1,6 @@
 // Date and time 
 let now =new Date();
 console.log("The current Date and time is: ",now)
+
+let startDate=new Date(0); // this give us Thu Jan 01 1970 05:00:00 GMT+0500 (Pakistan Standard Time)
+console.log("The starting time and date of this object is: ",startDate)
