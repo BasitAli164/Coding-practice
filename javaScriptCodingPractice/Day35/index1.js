04383d8d7fde4f6e79d8f20b80fb51f32f224a03
Basit Ali <opoali911@gmail.com>
2024-12-29 17:14:10 +0500

    explore the json formate

--- javaScriptCodingPractice/Day35/index1.js
+++ javaScriptCodingPractice/Day35/index1.js
@@ -2,7 +2,17 @@
 let user={
     userName:"Basit",
     age:23,
-    gender:'male'
+    status:undefined,
+    gender:'male',
+    phone:['1234','343434','343434'],
+    favBook:{
+        bookName:"Zarbekaleem",
+        author:"Dr. Iqbal",
+        publishDate:Date.now(),
+    },
+    work:function(){
+        console.log("Developer")
+    }
 }
 console.log("Without converting in json formate: ",user)
 let convertJSON=JSON.stringify(user);
@@ -11,3 +21,7 @@ console.log("After converting in Json Format: ",convertJSON)
 localStorage.setItem('userData',convertJSON);
 let getData=localStorage.getItem("userData")
 console.log("getData is: ",getData)
+
+
+let convertString=JSON.parse(convertJSON);
+console.log("Converting the Stringify into the parse is: ",convertString)
