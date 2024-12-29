f3b2a826897fe4efbb300263c5bdc23814ca0803
Basit Ali <opoali911@gmail.com>
2024-12-29 16:09:42 +0500

    start the JSON

--- javaScriptCodingPractice/Day35/index.js
+++ javaScriptCodingPractice/Day35/index.js
@@ -51,4 +51,13 @@ function getLastDayOfMonth(year, month) {
     let date = new Date(year, month + 1, 0);
     return date.getDate();
   }
-console.log(getLastDayOfMonth(2012, 1))
+console.log(getLastDayOfMonth(2012, 1));
+
+
+
+
+
+
+
+
+
