681d34f9c1eba5e0e26ea771fd7e9cd6f2dd2e4f
Basit Ali <opoali911@gmail.com>
2024-12-31 00:16:38 +0500

    explore the memo hook again

--- LearningReact/ReactPractice/src/App.jsx
+++ LearningReact/ReactPractice/src/App.jsx
@@ -1,25 +1,13 @@
 import React, { useState ,useCallback} from 'react'
 import './App.css'
-import UseCallBack from './components/ReactHook/UseCallBack'
+import UseReducer from './components/ReactHook/UseReducer'
 
 const App = () => {
-  console.log("Parent Component ")
-    const [count,setCount]=useState(0);
-    const [count1,setCount1]=useState(0);  
-
-    const incrementCount=useCallback(()=>{
-      setCount1((prev)=>prev+10)
-      console.log("Use call back function ")
-    },[])
+ 
   return (
     <>
-
-    <UseCallBack increment={incrementCount}/>
-    <p>Count1: {count}</p>
-    <button onClick={()=>setCount(count+1)}>Increment Count</button>
-
-
-    <p>Count2: {count1}</p>
+  <UseReducer/>
+   
     </>
   )
 }
