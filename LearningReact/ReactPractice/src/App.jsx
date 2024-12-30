8ade89e5fff7461db7bff4e1433bfeb4e802d4a5
Basit Ali <opoali911@gmail.com>
2024-12-30 11:24:41 +0500

    explore the reacursion mean again start function in js

--- LearningReact/ReactPractice/src/App.jsx
+++ LearningReact/ReactPractice/src/App.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState ,useCallback} from 'react'
 import './App.css'
-import UseRef from './components/ReactHook/UseRef'
+import UseCallBack from './components/ReactHook/UseCallBack'
 
 const App = () => {
+  console.log("Parent Component ")
+    const [count,setCount]=useState(0);
+    const [count1,setCount1]=useState(0);  
+
+    const incrementCount=useCallback(()=>{
+      setCount1((prev)=>prev+10)
+      console.log("Use call back function ")
+    },[])
   return (
     <>
-    <UseRef/>
+
+    <UseCallBack increment={incrementCount}/>
+    <p>Count1: {count}</p>
+    <button onClick={()=>setCount(count+1)}>Increment Count</button>
+
+
+    <p>Count2: {count1}</p>
     </>
   )
 }
