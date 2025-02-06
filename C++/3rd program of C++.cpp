df460cf22aa85e1d00c6c19fda2316359b04af0a
Basit Ali <opoali911@gmail.com>
2025-02-06 17:32:34 +0500

    working on the data type in c++

--- C++/3rd program of C++.cpp
+++ C++/3rd program of C++.cpp
@@ -5,8 +5,20 @@ int main(){
 	int age=23;
 	
 	cout<<"My age is:"<<age<<endl;
-	cout<<"The size of my variable age: "<<sizeof(age)<<endl;
 	char grade='A';
 	cout<<"My grade is:"<<grade<<endl;
+	float height=5.7;
+	cout<<"My height is:"<<height<<endl;
+	cout<<"and it types is:"<<sizeof(height);
+	
+	bool isSafe=false;
+	cout<<"is safe is: "<<isSafe<<endl;
+	
+	double pi=3.14434343;
+	cout<<"PI is:"<<pi<<endl;
+	
+		cout<<"The size of my variable age(int): "<<sizeof(age)<<endl<<"char grade is:"<<sizeof(grade)<<endl<<"float height is:"<<sizeof(height)<<endl<<
+		"is safe bool is:"<<sizeof(isSafe)<<endl<<"last thing is pi double:"<<sizeof(pi);
+
 	return 0;
 }
