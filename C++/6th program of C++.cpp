e7be8b7bdaaf01eae728a0a0d85fe2a1a634bf32
Basit Ali <opoali911@gmail.com>
2025-02-06 22:42:59 +0500

    explore the arithmetic , relational and logical operator in C++

--- C++/6th program of C++.cpp
+++ C++/6th program of C++.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 int main(){
 	
-	// Arithmetic operators
+	// Arithmetic Operators
 	int a=2,b=5;
+	cout<<"Arithmetic Operators"<<endl;
 	cout<<"sum: "<<(a+b)<<endl;
 	cout<<"sub: "<<(b-a)<<endl;
 	cout<<"product: "<<(a*b)<<endl;
@@ -12,13 +13,18 @@ int main(){
 	cout<<"division: "<<(b/(float)a)<<endl; // explore the type casting
 	cout<<"modulo: "<<(a%b)<<endl;
 	
-	// Relational operators
+	// Relational Operators
+	cout<<"Relational Operators"<<endl;
 	cout<<(3<5)<<endl; // true or 1
 	cout<<(3>=5)<<endl; // false or 0
 	cout<<(3<=3)<<endl; // true or 1
 	cout<<(3<=5)<<endl; // true or 1
 	cout<<(3==5)<<endl; // false or 0
 	cout<<(3!=5)<<endl; // true or 1
+	
+	// Logical Operators
+	cout<<"Logical Operators"<<endl;
+	cout<<!(2<1)<<endl; // true or 1
 	
 	return 0;
 }
