9afc8398ad514797e4b19a85f81a0cae84f13029
Basit Ali <opoali911@gmail.com>
2025-02-06 22:16:26 +0500

    working on the type casting in C++ language

--- C++/4th program of C++.cpp
+++ C++/4th program of C++.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 using  namespace std;
+//type casting in C++ 
+/* 
+i-Implicit: through compiler
+ii-explicit: through programmer and this must be from large to small data type
+*/
 int main(){
-	char grad ='A';
+	// implicit
+	char grad='a';
 	int change=grad;
 	cout<<change<<endl;
 	
+	// explicit
+	
+	double price=12.897;
+	float newPrice=int(price);
+	cout<<"The converting price is:"<<newPrice<<endl;
 	
 	
 	return 0;
