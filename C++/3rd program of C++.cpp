d704c61b01ad11f8cdb8b18d4295064b777aefb7
Basit Ali <opoali911@gmail.com>
2025-02-06 17:19:49 +0500

    now the operator or function in C++

--- C++/3rd program of C++.cpp
+++ C++/3rd program of C++.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
+	
 	int age=23;
+	
 	cout<<"My age is:"<<age<<endl;
+	cout<<"The size of my variable age: "<<sizeof(age)<<endl;
 	return 0;
 }
