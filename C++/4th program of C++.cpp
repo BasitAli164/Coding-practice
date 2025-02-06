98288870340918879d20453d0d6f22acaf48ba00
Basit Ali <opoali911@gmail.com>
2025-02-06 22:17:45 +0500

    explore new things in the C++ type casting

--- C++/4th program of C++.cpp
+++ C++/4th program of C++.cpp
@@ -6,7 +6,9 @@ i-Implicit: through compiler
 ii-explicit: through programmer and this must be from large to small data type
 */
 int main(){
+	
 	// implicit
+	
 	char grad='a';
 	int change=grad;
 	cout<<change<<endl;
@@ -14,7 +16,8 @@ int main(){
 	// explicit
 	
 	double price=12.897;
-	float newPrice=int(price);
+//	float newPrice=int(price);
+	float newPrice=(int)price; // line 19 and 20 are both same
 	cout<<"The converting price is:"<<newPrice<<endl;
 	
 	
