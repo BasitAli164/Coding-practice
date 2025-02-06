83eba62b9bdab3b43d0cadaa412cfb17cb1d7ace
Basit Ali <opoali911@gmail.com>
2025-02-06 22:40:03 +0500

    exploring different type of operator in c++

--- C++/6th program of C++.cpp
+++ C++/6th program of C++.cpp
@@ -8,9 +8,17 @@ int main(){
 	cout<<"sum: "<<(a+b)<<endl;
 	cout<<"sub: "<<(b-a)<<endl;
 	cout<<"product: "<<(a*b)<<endl;
-	cout<<"division: "<<(a/b)<<endl;
+	cout<<"division: "<<(b/a)<<endl;
+	cout<<"division: "<<(b/(float)a)<<endl; // explore the type casting
 	cout<<"modulo: "<<(a%b)<<endl;
 	
+	// Relational operators
+	cout<<(3<5)<<endl; // true or 1
+	cout<<(3>=5)<<endl; // false or 0
+	cout<<(3<=3)<<endl; // true or 1
+	cout<<(3<=5)<<endl; // true or 1
+	cout<<(3==5)<<endl; // false or 0
+	cout<<(3!=5)<<endl; // true or 1
 	
 	return 0;
 }
