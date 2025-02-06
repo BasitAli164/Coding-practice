030d1ef0afa555ff19e60d78697bb1933ceec990
Basit Ali <opoali911@gmail.com>
2025-02-06 23:11:48 +0500

    enough for today with c++

--- C++/7th program of C++cpp.cpp
+++ C++/7th program of C++cpp.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a,b,sum=0;
+	int a,b;
 	cout<<"Enter a: "<<ends;
 	cin>>a;
 	cout<<"Enter b: "<<ends;
 	cin>>b;
-	sum=a+b;
+	int sum=a+b;
+	int product=a*b;
+	int minus=a-b;
+	int divide=a/b;
+	int modulo=a%b;
+	
+	cout<<"Operation are: "<<endl;
 	cout<<"Sum is: "<<sum<<endl;
+	cout<<"Product is: "<<product<<endl;
+	cout<<"Subtraction is: "<<minus<<endl;
+	cout<<"Divide is: "<<divide<<endl;
+	cout<<"Module is: "<<modulo<<endl;
 	
 	return 0;
 }
