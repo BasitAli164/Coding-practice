4662ea2678d2d285ebcb7144832fbc050eacb129
Basit Ali <opoali911@gmail.com>
2025-02-06 22:30:19 +0500

    explore the operators in c++

---
+++ C++/6th program of C++.cpp
@@ -0,0 +1,16 @@
+// operaters in C++
+#include<iostream>
+using namespace std;
+int main(){
+	
+	// Arithmetic operators
+	int a=2,b=5;
+	cout<<"sum: "<<(a+b)<<endl;
+	cout<<"sub: "<<(b-a)<<endl;
+	cout<<"product: "<<(a*b)<<endl;
+	cout<<"division: "<<(a/b)<<endl;
+	cout<<"modulo: "<<(a%b)<<endl;
+	
+	
+	return 0;
+}
