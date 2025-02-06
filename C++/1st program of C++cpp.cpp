f98c28360ecacf480a7aae792d76c070483c18ed
Basit Ali <opoali911@gmail.com>
2025-02-06 17:03:34 +0500

    start coding in C++

---
+++ C++/1st program of C++cpp.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Hello world!"<<endl;
+	cout<<"I am Basit Ali"<<"\n";
+	cout<<"I am from Pakistan";
+	
+	return 0;
+}
---
+++ C++/2nd program of C++cpp.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"First Name: "<<"Basit"<<endl<<"Last Name: "<<"Ali";
+	return 0;
+}
