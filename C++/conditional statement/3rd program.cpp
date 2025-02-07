aadd27c7b3789e0aa87e5446072a220af443d46c
Basit Ali <opoali911@gmail.com>
2025-02-07 16:55:43 +0500

    woking on the ternary operator

---
+++ C++/conditional statement/3rd program.cpp
@@ -0,0 +1,11 @@
+// ternary operator
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter a number"<<ends;
+	cin>>n;
+	cout<<(n>0?"Positive":"Negative")<<endl;
+	
+	return 0;
+}
