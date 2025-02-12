674d860fc0041d772f5a240524fbea2255716d5e
Basit Ali <opoali911@gmail.com>
2025-02-12 14:43:40 +0500

    working with ts

--- C++/basic and operators/1st program of C++cpp.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	cout<<"Hello world!"<<endl;
-	cout<<"I am Basit Ali"<<"\n";
-	cout<<"I am from Pakistan";
-	
-	return 0;
-}
--- C++/basic and operators/2nd program of C++cpp.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	cout<<"First Name: "<<"Basit"<<endl<<"Last Name: "<<"Ali";
-	return 0;
-}
