33ce15721b146f0e75f70d4185cbef3810aa09a8
Basit Ali <opoali911@gmail.com>
2025-02-12 22:25:38 +0500

    working with the typeScript

--- typeScript/ts/app.js
+++ typeScript/ts/app.js
@@ -1,10 +1,2 @@
 "use strict";
-var a = 2;
-var b = 343;
-let marks = 34;
-let d;
-function hellow(hello) {
-    console.log(hello);
-}
-hellow("Basit");
-a = 90;
+// data type in typescript
--- typeScript/ts/app.ts
+++ typeScript/ts/app.ts
@@ -1,9 +1,1 @@
-var a=2;
-var b=343;
-let marks:number=34;
-let d;
-function hellow(hello:string){
-    console.log(hello)
-}
-hellow("Basit")
-a=90;
+// data type in typescript
