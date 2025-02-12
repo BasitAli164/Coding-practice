a303f6c8831e535507c04715248b21381ad0447f
Basit Ali <opoali911@gmail.com>
2025-02-12 13:27:01 +0500

    exploring the data type in typescript

--- typeScript/app.js
+++ typeScript/app.js
@@ -3,3 +3,7 @@ var a = 2;
 var b = 343;
 let marks = 34;
 let d;
+function hellow(hello) {
+    console.log(hello);
+}
+hellow("Basit");
--- typeScript/app.ts
+++ typeScript/app.ts
@@ -2,3 +2,7 @@ var a=2;
 var b=343;
 let marks:number=34;
 let d;
+function hellow(hello:string){
+    console.log(hello)
+}
+hellow("Basit")
