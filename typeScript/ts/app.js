0d1dce4a344f0209a63cfa01a5d95287ae053d9d
Basit Ali <opoali911@gmail.com>
2025-02-12 22:28:35 +0500

    explore the datatype in typescript

--- typeScript/ts/app.js
+++ typeScript/ts/app.js
@@ -1,2 +1,8 @@
 "use strict";
 // data type in typescript
+let num = 34;
+console.log("num is:", num);
+let num1 = 4545454;
+console.log("num1 is:", num1);
+let num2 = 324324324234234n;
+console.log("num2 is: ", num2);
--- typeScript/ts/app.ts
+++ typeScript/ts/app.ts
@@ -1,1 +1,8 @@
 // data type in typescript
+let num:number=34;
+console.log("num is:",num)
+let num1=4545454;
+console.log("num1 is:",num1)
+
+let num2=324324324234234n;
+console.log("num2 is: ",num2)
