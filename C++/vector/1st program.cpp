2902294d2f258f9b13bf78a6667fb1034a860942
Basit Ali <opoali911@gmail.com>
2025-02-13 16:32:17 +0500

    working with vector in C++

---
+++ C++/vector/1st program.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	
+	vector<int>vec={2,3,4,5,6};
+	for(int item:vec){ //this is the syntax of the for each loop in c++ i think
+		cout<<item<<ends;
+	}
+	
+	
+	return 0;
+}
