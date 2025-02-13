b4beebd30495f3b2eac1951478566e7e9149dcf8
Basit Ali <opoali911@gmail.com>
2025-02-13 16:35:35 +0500

    exploring the vector and its related topics

--- C++/vector/1st program.cpp
+++ C++/vector/1st program.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int main(){
 	
 	vector<int>vec={2,3,4,5,6};
-	for(int item:vec){ //this is the syntax of the for each loop in c++ i think
+	cout<<"The starting value of the vector is: "<<vec.front()<<endl;
+	cout<<"The last value in the vector is: "<<vec.back()<<endl;
+	vec.push_back(90);
+	vec.push_back(34);
+	vec.push_back(292);
+	
+	for(int item:vec){ //this is the syntax of the for each loop in c++ i think 
 		cout<<item<<ends;
 	}
 	
