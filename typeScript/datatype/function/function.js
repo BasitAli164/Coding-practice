6e7ec5d5806aad351922c01ebcfa0b9cd96b238c
Basit Ali <opoali911@gmail.com>
2025-02-16 14:36:42 +0500

    exploring the typesScript in depth

--- typeScript/datatype/function/function.js
+++ typeScript/datatype/function/function.js
@@ -1,2 +1,12 @@
 "use strict";
-//! function in typeScrpt
+//! function return type in typeScrpt
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log("Sum of two number is: ", sum(34, 6));
+function display(msg) {
+    console.log(msg);
+}
+display("This is the message...");
+let functionType;
+functionType = (() => (console.log("object")));
--- typeScript/datatype/function/function.ts
+++ typeScript/datatype/function/function.ts
@@ -1,1 +1,14 @@
-//! function in typeScrpt
+//! function return type in typeScrpt
+
+function sum(num1:number,num2:number):number{
+    return num1+num2;
+}
+console.log("Sum of two number is: ",sum(34,6));
+
+function display(msg:string):void{
+    console.log(msg)
+}
+display("This is the message...")
+
+let functionType:Function;// now the variable functionType store only function not any other datatype so don't put any other datatype like number , string, boolean ,array,object etc....
+functionType=()=>{}
