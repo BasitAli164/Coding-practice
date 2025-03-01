ac27ba12aa886068cf8bb334b9e07d0f41fee72a
Basit Ali <opoali911@gmail.com>
2025-03-01 23:02:12 +0500

    wow...

--- Nodejs/crud/delFile.js
+++ Nodejs/crud/delFile.js
@@ -1,4 +1,4 @@
 const fs=require("fs");
 // const delete=fs.unlinkSync("./text.txt"); 
 // the variable create error to solve this we don't store it in any variable 
-fs.unlinkSync("./text.txt") // through this i delete the text.txt files
+fs.unlinkSync("./text.txt") // through this i delete the text.txt files permanently ,don't go in recycle or distbin
