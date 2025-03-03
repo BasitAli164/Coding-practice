6aca1940592c3a3df9b28dd62632d89d8f5bdb0c
Basit Ali <opoali911@gmail.com>
2025-03-03 17:24:58 +0500

    finished the process properties know i am start to explore the methods of the process

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,4 +1,9 @@
-console.log("hi")
+// Properties of the process object
 // console.log("Porocess is:",process)
 // console.log("Porocess arg is:",process.argv)
-console.log("Porocess env is:",process.env)
+// console.log("Porocess env is:",process.env)
+// console.log("Porocess version is:",process.version)
+// console.log("Porocess arch is:",process.arch)
+
+
+// Method of the process object
