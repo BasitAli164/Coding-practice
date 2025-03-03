6ca6ef04859cd06e3fc77de50d52e51c54d0cb7f
Basit Ali <opoali911@gmail.com>
2025-03-03 17:21:56 +0500

    explore the method of the process object

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,2 +1,4 @@
 console.log("hi")
-console.log("Porocess is:",process)
+// console.log("Porocess is:",process)
+// console.log("Porocess arg is:",process.argv)
+console.log("Porocess env is:",process.env)
