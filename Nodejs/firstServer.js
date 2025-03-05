17d06fd7b15f109060973771eacac77aa9cce1d1
Basit Ali <opoali911@gmail.com>
2025-03-05 08:38:09 +0500

    make a server and two route and also handle error route in pure node js

--- Nodejs/firstServer.js
+++ Nodejs/firstServer.js
@@ -1,7 +1,43 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log("request on server")
+    // console.log("request on server",req) // this give  whole object
+    console.log("Url is: ",req.url);
+    console.log("Method is: ",req.method);
+    console.log("Header is: ",req.headers);
+
+    if(req.url==='/'){
+        res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>Home Page</title></head>')
+    res.write('<body><h2>Hello world ! I am learning the node js</h2></body>')
+    res.write('</html>')
+    return res.end();
+        
+    }else if(req.url.toLowerCase()==='/skills'){
+        res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>Skill Page</title></head>')
+    res.write('<body><h2>React ,Next js ,Mongodb </h2></body>')
+    res.write('</html>')
+    return res.end();
+
+    }else{
+        res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>Error Page</title></head>')
+    res.write('<body><h2>404</h2></body>')
+    res.write('</html>')
+    return res.end();
+
+    }
+    
+  
+    
+        
+
+    
+    // process.exit();// this is used to stop the server , this name is event loop 
 })
 
 server.listen(2000,()=>{
