3d11a23e92b3185099cea402bb57e96ca3dba338
Basit Ali <opoali911@gmail.com>
2025-03-06 08:22:50 +0500

    make a form in node js

--- Nodejs/secondServer.js
+++ Nodejs/secondServer.js
@@ -5,6 +5,28 @@ const server=http.createServer((req,res)=>{
     setTimeout(()=>{
         res.end()
     },2000)
+    if(req.url==='/'){
+        res.setHeader("Content-Type","text/html")
+        res.write("<html>")
+        res.write("<head>")
+        res.write("<title>Form with Node js</title>")
+        res.write("</head>")
+        res.write("<body>")
+        res.write('<h1>Welcome to Home Page..</h1>')
+        res.write('<form action="/submit-details" method="POST">')
+        res.write("<input type='text' id='name' name='name' placeholder='Enter your name'/><br/><br/>")
+        res.write("<label for='gender'>Gender</lable> <br/><br/>")
+        res.write("<input type='radio' id='male' name='gender ' value='male'/>")
+        res.write("<label for='male'>Male</lable>")
+        res.write("<input type='radio' id='female' name='gender ' value='female'/>")
+        res.write("<label for='female'>Female</lable><br/><br/>")
+        res.write("<button type='submit'>Submit</button>")
+        res.write("<form/>")
+        res.write("<body/>")
+        res.write("<html/>")
+        return res.end();
+
+    }
 })
 
 const port=4321;
