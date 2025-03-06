06ad17ca0c310aa9f3bcdfc6d10cb52a9b8b534b
Basit Ali <opoali911@gmail.com>
2025-03-06 08:58:43 +0500

    working with node js

--- Nodejs/firstServer.js
+++ Nodejs/firstServer.js
@@ -1,46 +1,45 @@
-const http=require('http');
+const http = require("http");
 
-const server=http.createServer((req,res)=>{
-    // console.log("request on server",req) // this give  whole object
-    console.log("Url is: ",req.url);
-    console.log("Method is: ",req.method);
-    console.log("Header is: ",req.headers);
+const server = http.createServer((req, res) => {
+  // console.log("request on server",req) // this give  whole object
+  // console.log("Url is: ",req.url);
+  // console.log("Method is: ",req.method);
+  // console.log("Header is: ",req.headers);
 
-    if(req.url==='/'){
-        res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head><title>Home Page</title></head>')
-    res.write('<body><h2>Hello world ! I am learning the node js</h2></body>')
-    res.write('</html>')
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<head><title>Form in Node js</title></head>");
+    res.write("<body>");
+    res.write("<h1>Welcome to Home Page..</h1>");
+    res.write('<form action="/submitform" method="POST">');
+    res.write(
+      "<input type='text' id='name' name='name' placeholder='Enter your name'/><br/><br/>"
+    );
+    res.write("<label for='gender'>Gender</lable> <br/><br/>");
+    res.write("<input type='radio' id='male' name='gender ' value='male'/>");
+    res.write("<label for='male'>Male</lable>");
+    res.write(
+      "<input type='radio' id='female' name='gender ' value='female'/>"
+    );
+    res.write("<label for='female'>Female</lable><br/><br/>");
+    res.write("<button type='submit'>Submit</button>");
+    res.write("<form/>");
+    res.write("<body/>");
+    res.write("<html/>");
     return res.end();
-        
-    }else if(req.url.toLowerCase()==='/skills'){
-        res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head><title>Skill Page</title></head>')
-    res.write('<body><h2>React ,Next js ,Mongodb </h2></body>')
-    res.write('</html>')
+  } else if (req.url.toLowerCase() === "/submitfrom" && req.method==="POST") {
+    res.write("<head><title>Submit Form</title></head>");
+    res.write("<body>");
+    res.write("<h1>Hello, Everyone I make a simple form by using Node JS</h1>");
+    res.write("<body/>");
     return res.end();
+  } else {
+    res.write("<body><h2>Not-Found 404</h2></body>");
+  }
 
-    }else{
-        res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head><title>Error Page</title></head>')
-    res.write('<body><h2>404</h2></body>')
-    res.write('</html>')
-    return res.end();
+  // process.exit();// this is used to stop the server , this name is event loop
+});
 
-    }
-    
-  
-    
-        
-
-    
-    // process.exit();// this is used to stop the server , this name is event loop 
-})
-
-server.listen(2000,()=>{
-    console.log("server is runing on http://localhost:2000");
-    
-})
+server.listen(2000, () => {
+  console.log("server is runing on http://localhost:2000");
+});
--- Nodejs/secondServer.js
+++ Nodejs/secondServer.js
@@ -1,37 +1,52 @@
-const http=require('http');
+const http = require("http");
 
-const server=http.createServer((req,res)=>{
-    res.write("<h1>Hello Every One this is my Second server in Node js</h1>")
-    setTimeout(()=>{
-        res.end()
-    },2000)
-    if(req.url==='/'){
-        res.setHeaders("Content-Type","text/html")
-        res.write("<html>")
-        res.write("<head>")
-        res.write("<title>Form with Node js</title>")
-        res.write("</head>")
-        res.write("<body>")
-        res.write('<h1>Welcome to Home Page..</h1>')
-        res.write('<form action="/submit-details" method="POST">')
-        res.write("<input type='text' id='name' name='name' placeholder='Enter your name'/><br/><br/>")
-        res.write("<label for='gender'>Gender</lable> <br/><br/>")
-        res.write("<input type='radio' id='male' name='gender ' value='male'/>")
-        res.write("<label for='male'>Male</lable>")
-        res.write("<input type='radio' id='female' name='gender ' value='female'/>")
-        res.write("<label for='female'>Female</lable><br/><br/>")
-        res.write("<button type='submit'>Submit</button>")
-        res.write("<form/>")
-        res.write("<body/>")
-        res.write("<html/>")
-        return res.end();
-
-    }
-})
-
-const port=4321;
-
-server.listen(port,()=>{
-    console.log(`Server is Runing at http://localhost:${port}`);
+const server = http.createServer((req, res) => {
+  res.write("<h1>Hello Every One this is my Second server in Node js</h1>");
+  setTimeout(() => {
+    res.end();
+  }, 2000);
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Form in Node js</title></head>");
+    res.write("<body>");
+    res.write("<h1>Welcome to Home Page..</h1>");
+    res.write('<form action="/submitform" method="POST">');
+    res.write(
+      "<input type='text' id='name' name='name' placeholder='Enter your name'/><br/><br/>"
+    );
+    res.write("<label for='gender'>Gender</lable> <br/><br/>");
+    res.write("<input type='radio' id='male' name='gender ' value='male'/>");
+    res.write("<label for='male'>Male</lable>");
+    res.write(
+      "<input type='radio' id='female' name='gender ' value='female'/>"
+    );
+    res.write("<label for='female'>Female</lable><br/><br/>");
+    res.write("<button type='submit'>Submit</button>");
+    res.write("<form/>");
+    res.write("<body/>");
+    res.write("<html/>");
+    return res.end();
+  }else if (req.method=="POST" && req.url.toLowerCase() =="/submitfrom") {
+   
+    res.write("<head><title>Submit Form</title></head>");
+    res.write("<body>");
+    res.write("<h1>Hello, Everyone I make a simple form by using Node JS</h1>");
+    res.write("<body/>");
+   
+    return res.end();
+  } else {
     
-})
+    res.write("<head><title>Error</title></head>");
+    res.write("<body>");
+    res.write("<h1>Not-Found 404</h1>");
+    res.write("<body/>");
+  
+    // return res.end();
+  }
+});
+
+const port = 4351;
+
+server.listen(port, () => {
+  console.log(`Server is Runing at http://localhost:${port}`);
+});
