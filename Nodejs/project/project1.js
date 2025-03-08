2a7e48e7d152b580d5c0c337c547d7cd4e91ca43
Basit Ali <opoali911@gmail.com>
2025-03-08 06:50:16 +0500

    working

--- Nodejs/project/project1.js
+++ Nodejs/project/project1.js
@@ -5,9 +5,27 @@ const server=http.createServer((req,res)=>{
         res.write("<html>")
         res.write("<head><title>Home Page</title></head>")
         res.write("<body>")
-        res.write("<h1>...Home Page...</h1>")
+        res.write("<h1 id='home'>...Home Page...</h1>")
+        res.write("<a href='#home'>Home</a>")
+        res.write("<a href='#men'>Men</a>")
+        res.write("<a href='#women'>Wome</a>")
+        res.write("<a href='#kids'>Kids</a>")
+        res.write("<a href='#carts'>Cart</a>")
         res.write("</body>")
-        res.write("<")
+        res.write("</html>")
+        return res.end();
+    }else if(req.url==="#men"){
+        res.setHeader("Content-Type","html/text")
+        res.write("<html>")
+        res.write("<head><title>Men Page</title></head>")
+        res.write("<body>")
+        res.write("<h1 id='home'>...Men Page...</h1>")
+        res.write("</body>")
+        res.write("</html>")
+        return res.end()
+
+    }else{
+        res.end();
     }
     
 })
