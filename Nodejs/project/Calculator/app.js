ce8728df408497413fc9ca3c8436891b4db78334
Basit Ali <opoali911@gmail.com>
2025-03-09 07:34:10 +0500

    create server for calculator

---
+++ Nodejs/project/Calculator/app.js
@@ -0,0 +1,12 @@
+// Calculator
+
+ const http=require("http")
+
+ const server=http.createServer((req,res)=>{
+
+ })
+const port=4000;
+ server.listen(port, ()=>{
+    console.log(`Server is running at http://localhost: ${port}`)
+ })
+  
