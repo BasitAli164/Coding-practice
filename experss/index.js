70b956e749cb6bb0014f8b5df575c06477e45931
Basit Ali <opoali911@gmail.com>
2025-03-13 06:59:02 +0500

    make controller file to handle the handler in of routes logic

--- experss/index.js
+++ experss/index.js
@@ -1,32 +1,28 @@
-import express from 'express'
-const app=express();
+import express from "express";
+import { searchController, usernameController } from "./controller.js";
+const app = express();
 
-const port=3000;
+const port = 3000;
 
 // Define a simple route
 
-app.get("/",(req,res)=>{
-    console.log(req.method )
-    res.send("Hello Experss, this is the first response by using express js in backend series")
-})
-app.get("/about",(req,res)=>{
-    res.send("This is about page")
-})
-app.get("/contact",(req,res)=>{
-    res.send("This is the contact page")
-})
-
+app.get("/", (req, res) => {
+  console.log(req.method);
+  res.send(
+    "Hello Experss, this is the first response by using express js in backend series"
+  );
+});
+app.get("/about", (req, res) => {
+  res.send("This is about page");
+});
+app.get("/contact", (req, res) => {
+  res.send("This is the contact page");
+});
 
 // Dynamic Route using params(:)
-app.get('/user/:username',(req,res)=>{
-    const username=req.params.username;
-    res.send(`Welcom ${username}`)
-})
+app.get("/user/:username", usernameController);
 // Dynamic Route using Query(?)
-app.get('/search',(req,res)=>{
-    const keyword=req.query.keyword
-    res.send(`Searching of ${keyword}`)
-})
-app.listen(port, ()=>{
-    console.log(`The server is running at http://localhost:${port}`)
-})
+app.get("/search", searchController);
+app.listen(port, () => {
+  console.log(`The server is running at http://localhost:${port}`);
+});
