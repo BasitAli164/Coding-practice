dbcba62350f60f2a0e57fd6a72451de3ea47becb
Basit Ali <opoali911@gmail.com>
2025-03-14 06:22:35 +0500

    know introduce the middleware in the express js

--- express/index2.js
+++ express/index2.js
@@ -2,11 +2,18 @@ import express from 'express'
 
 const app=express();
 
+
+//! Middleware
+app.use((req,res,next)=>{
+    console.log("A new request received at"+Date.now()) 
+    next()
+})
 app.get('/',(req,res)=>{
+    console.log("object")
     res.send("Now, Again working on Experss js and this is second experss server")
 })
 
-//!  multiple route parameter(multiple params in url)
+//?  multiple route parameter(multiple params in url)
 app.get('/things/:name/:id',(req,res)=>{
     const {name,id}=req.params;
     res.json({
@@ -17,8 +24,8 @@ app.get('/things/:name/:id',(req,res)=>{
 
 })
 
-//! Using regx in url with params 
-app.get('/product/:name/:id([0-9]{5})',(req,res)=>{ //? Here i use regx and this regx work is if id should be 5 digit and must be a number , if don't provide then we get error
+//? Using regx in url with params 
+app.get('/product/:name/:id([0-9]{5})',(req,res)=>{ // Here i use regx and this regx work is if id should be 5 digit and must be a number , if don't provide then we get error
     const {name,id}=req.params;
     res.json({
         message:"Get Data form URL",
@@ -27,7 +34,10 @@ app.get('/product/:name/:id([0-9]{5})',(req,res)=>{ //? Here i use regx and this
     })
 
 })
-
+//? Catch all invalid route
+app.get('*',(req,res)=>{
+    res.send("Sorry this is an invalid url")
+})
 
 //! Port
 const port=3001;
