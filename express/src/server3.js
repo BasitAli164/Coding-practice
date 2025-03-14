6f56750c8f4eb346bd2af5776a4ee48d512ab255
Basit Ali <opoali911@gmail.com>
2025-03-14 10:34:22 +0500

    enough for today the express js

--- express/src/server3.js
+++ express/src/server3.js
@@ -1,4 +1,4 @@
-//! Here we explore the concept of the middleware in expressjs
+//! Here we explore or focus the concept of the middleware in expressjs
 import express from 'express'
 import dotenv from 'dotenv'
 
@@ -6,6 +6,19 @@ import dotenv from 'dotenv'
 const app=express();
 dotenv.config()
 
+
+//! Types of Middleware
+
+//? 1- Application Level Middleware
+
+app.use((req,res,next)=>{
+    console.log("A new request accepting  at",+Date.now())
+    next()// this middleware run on whole application 
+})
+
+app.get('/',(req,res)=>{
+    res.send("Hi! I am 3rd Server")
+})
 app.listen(process.env.PORT3,()=>{
     console.log(`Server is running at http://localhost:${process.env.PORT3}`)
 
