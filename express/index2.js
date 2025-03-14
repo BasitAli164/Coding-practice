3e84d53335220a70144aa872c9eb1003a9c300af
Basit Ali <opoali911@gmail.com>
2025-03-14 05:42:25 +0500

    know working on the multiple route parameter means handling multiple parameter (params) in url

--- express/index2.js
+++ express/index2.js
@@ -6,11 +6,14 @@ app.get('/',(req,res)=>{
     res.send("Now, Again working on Experss js and this is second experss server")
 })
 
-
+//!  multiple route parameter(multiple params in url)
+app.get('/things/:name/:id',(req,res)=>{
+    
+})
 
 
 //! Port
-const port=3000;
+const port=3001;
 //! Server is Listening
 app.listen(port,()=>{
     console.log(`Server is listening at http://localhost:${port}`)
