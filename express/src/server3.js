5df00574505c83bda06f4c3f78943ba4e407abc5
Basit Ali <opoali911@gmail.com>
2025-03-14 06:51:25 +0500

    fixing errors

--- express/src/server3.js
+++ express/src/server3.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app=express();
 
-app.listen(port,()=>{
+app.listen(process.env.PORT3,()=>{
     console.log(`Server is running at http://localhost:${process.env.PORT3}`)
 
 })
