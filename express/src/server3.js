3ad78d062af9760d2f5425713930f144066d813a
Basit Ali <opoali911@gmail.com>
2025-03-17 05:45:01 +0500

    make a middle ware for specifice route

--- express/src/server3.js
+++ express/src/server3.js
@@ -9,16 +9,30 @@ dotenv.config()
 
 //! Types of Middleware
 
-//? 1- Application Level Middleware
 
-app.use((req,res,next)=>{
+
+app.use((req,res,next)=>{ // this middleware is applied on all middleware
     console.log("A new request accepting  at",+Date.now())
     next()// this middleware run on whole application 
 })
 
+
 app.get('/',(req,res)=>{
     res.send("This is the 3rd server")
 })
+
+//? 1- Application Level Middleware
+app.use('/welcom',(req,res,next)=>{
+    console.log('A new Request at the welcom route at '+Date.now())
+    next();
+
+})
+app.get('/welcom',(req,res)=>{
+    res.send("Welcom to the Express Course/App ⚒️⚒️⚒️⚒️⚒️")
+})
+
+
+
 app.listen(process.env.PORT3,()=>{
     console.log(`Server is running at http://localhost:${process.env.PORT3}`)
 
