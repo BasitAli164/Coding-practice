98fb004bdb69a41b9faa9ab48a083786a7f31197
Basit Ali <opoali911@gmail.com>
2025-03-17 06:30:18 +0500

    I have some confusion in https and http and exploring this concept in deep in few days

--- express/src/server4.js
+++ express/src/server4.js
@@ -1,15 +1,16 @@
-import express from 'express';
-
-const app=express();
-
+import express from "express";
+import dotenv from 'dotenv';
 
 
 
-
-
+const app = express();
+dotenv.config()
+app.get("/", (req, res) => {
+  res.send("I am talking from Server Four");
+});
 
 //! Server Listening
 
 app.listen(process.env.PORT4,()=>{
-    console.log(`Server is runngin at ${process.env.PORT4}`)
+    console.log(`Server is running at http://localhost:${process.env.PORT4}`)
 })
