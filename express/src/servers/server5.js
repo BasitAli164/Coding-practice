54dd50878fc43cb8445c949895387a7ff2d51ce6
Basit Ali <opoali911@gmail.com>
2025-03-18 06:48:40 +0500

    exploring new things in the express

--- express/src/servers/server5.js
+++ express/src/servers/server5.js
@@ -3,12 +3,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 const app=express();
-dotenv.config();
 
+dotenv.config();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}))// this code work is parse the url data into json formate
 app.get('/',(req,res)=>{
     res.send("Hi ! I am talking from server 5")
+    res.render('form')
 })
 
+app.post('/form',(req,res)=>{
+    console.log(req.body)
+    
+})
 
 
 
