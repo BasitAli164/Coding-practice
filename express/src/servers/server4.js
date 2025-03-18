61784f72422e8b87bcb31e546076dc534cc1f56d
Basit Ali <opoali911@gmail.com>
2025-03-18 05:56:17 +0500

    fix the problem / error and bugs in express app

--- express/src/servers/server4.js
+++ express/src/servers/server4.js
@@ -14,7 +14,7 @@ app.set('view engine','ejs') // here we define our engine
 
 
 app.get("/", (req, res) => {
-  const username="Khatija Batool"
+  const username=" Batool"
   res.render('index',{username:username})
 });
 
