fca44b006f4a4e9f4eff25b64221dab7a920a042
Basit Ali <opoali911@gmail.com>
2025-03-18 05:59:08 +0500

    fix the bugs

--- express/src/servers/server4.js
+++ express/src/servers/server4.js
@@ -1,25 +1,22 @@
 //! Here we use the ejs( emmeded javascript files)
 import express from "express";
-import dotenv from 'dotenv';
-
+import dotenv from "dotenv";
 
 //? time 1:18 min
 
 const app = express();
-dotenv.config()
-
+dotenv.config();
 
 //? Set EJS as the view engine
-app.set('view engine','ejs') // here we define our engine
-
+app.set("view engine", "ejs"); // here we define our engine
 
 app.get("/", (req, res) => {
-  const username=" Batool"
-  res.render('index',{username:username})
+  const username = " Basit";
+  res.render("index", { username: username });
 });
 
 //! Server Listening
 
-app.listen(process.env.PORT4,()=>{
-    console.log(`Server is running at http://localhost:${process.env.PORT4}`)
-})
+app.listen(process.env.PORT4, () => {
+  console.log(`Server is running at http://localhost:${process.env.PORT4}`);
+});
