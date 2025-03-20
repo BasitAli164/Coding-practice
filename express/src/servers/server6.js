7dbc62f5590849eec88c48046bc199502f1e2152
Basit Ali <opoali911@gmail.com>
2025-03-20 05:46:03 +0500

    know working with multer

--- express/src/servers/server6.js
+++ express/src/servers/server6.js
@@ -2,10 +2,15 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import multer from 'multer'
 
 const app=express();
+const upload=multer()
 dotenv.config();
-app.use(express.static('public'))
+
+app.use(express.static('public'))// this is for fetching static data form public folder
+app.use(upload.array())// Here i use the multer functionality and use the array to catch multipart data in it
+
 app.get('/',(req,res)=>{
     res.send("Hi! I am talking from server 6")
 })
