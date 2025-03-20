30b88f7fb30592a46f11c9fa0eb5c69e82f7b3f0
Basit Ali <opoali911@gmail.com>
2025-03-20 05:35:26 +0500

    make the server 6

---
+++ express/src/servers/server6.js
@@ -0,0 +1,17 @@
+//! Here we dealing with form handling(multipart form handling) though third library like multer
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app=express();
+dotenv.config();
+app.use(express.static({}))
+app.get('/',(req,res)=>{
+    res.send("Hi! I am talking from server 6")
+})
+
+
+
+app.listen(process.env.PORT6,()=>{
+    console.log(`Server is runing at http://localhost:${process.env.PORT6}`)
+})
