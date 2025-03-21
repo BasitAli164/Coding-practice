7168f0371279879527f7fd23cba39d300c4af8ee
Basit Ali <opoali911@gmail.com>
2025-03-21 07:57:24 +0500

    make the server 8 ,working on db connection and create the dotgitIgnore file

---
+++ express/src/servers/server8.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app=express();
+dotenv.config();
+
+
+app.get('/',(req,res)=>{
+    res.send("Talking from server 7")
+})
+
+
+
+
+
+app.listen(process.env.PORT8,()=>{
+    console.log(`Server is running at http://localhost:3007`)
+    dbConnection()
+})
