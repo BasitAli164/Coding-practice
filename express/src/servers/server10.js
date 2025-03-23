0dc87709e91a9c642f96b13b201dbd9f7a7dfffb
Basit Ali <opoali911@gmail.com>
2025-03-23 06:11:08 +0500

    write the code of 10th server

---
+++ express/src/servers/server10.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import dbConnection3 from '../../config/db/dbConnection3';
+
+const app=express();
+dotenv.config();
+
+
+app.get('/',(req,res)=>{
+    res.send("I am talking from the server 10")
+})
+
+const port=process.env.PORT10 || 4332;
+
+dbConnection3().then(()=>{
+    app.listen(port, ()=>{
+        console.log(`Server runing at http://localhost:${port}`)
+    })
+}).catch((error)=>{
+    console.error(`Server failed to connect , due to this error ${error}`)
+})
