f21c54398724a6b3ead4e918df8a34b7fbe8fc8f
Basit Ali <opoali911@gmail.com>
2025-03-23 05:56:09 +0500

    completely write the third db code

--- express/config/db/dbConnection3.js
+++ express/config/db/dbConnection3.js
@@ -2,3 +2,22 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 
+dotenv.config();
+
+const dbConnection3=async()=>{
+    try {
+        await mongoose.connect(process.env.URI)
+        console.log(`Database successfully connected...`)
+        
+    } catch (error) {
+        console.error(`MongoDB error: ${error}`)
+                
+    }
+    mongoose.connection.on("Disconnected",()=>{
+        console.log("Database Disconnected")
+    })
+    
+
+}
+
+export default dbConnection3;
