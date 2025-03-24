f176c96a1925dbeede413cb173f4fa5a22de2f49
Basit Ali <opoali911@gmail.com>
2025-03-24 06:06:29 +0500

    hit properly the data route from post man

--- express/src/servers/server11.js
+++ express/src/servers/server11.js
@@ -6,10 +6,19 @@ import dbConnect from '../../config/db/dbConnection4.js'
 const app=express();
 dotenv.config();
 
-
+app.use(express.json())//! this is used to parse the data 
 app.get('/',(req,res)=>{
     res.send('I am talking form the server 11')
 })
+
+app.post('/adduser',(req,res)=>{
+    console.log(req.body)
+    res.json({
+        message:"User Add Successfully...."
+    })
+})
+
+
 
 
 const port=process.env.PORT11 ||4343
