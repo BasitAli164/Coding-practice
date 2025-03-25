9a0f52e94a8b9fd919fd498149569d835dfa6631
Basit Ali <opoali911@gmail.com>
2025-03-25 07:46:29 +0500

    start working in session by using express

--- express/src/servers/server13.js
+++ express/src/servers/server13.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
-
+import cookieParser from 'cookie-parser';
 
 const app=express();
 dotenv.config();
-
+app.use(cookieParser())// This middleware use to cookieParser
 
 
 app.get('/',(req,res)=>{
