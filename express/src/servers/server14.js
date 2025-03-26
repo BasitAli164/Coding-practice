be8399407bfa5307016d357ce3c4cf8f5042beea
Basit Ali <opoali911@gmail.com>
2025-03-26 05:51:07 +0500

    server14 is ready

---
+++ express/src/servers/server14.js
@@ -0,0 +1,37 @@
+//! Here we explore the Authentication process in ExpressJS 
+
+//? We can handle the authentication in express through tow method 
+// 1. Session-Based Authentication (Use cookies and session)
+// 2. Token-Based Authentication (Use JWT or OAuth tokens)
+import express from 'express';
+import dotenv from 'dotenv';
+import dbConnect from '../config/db/dbConnection5.js';
+
+
+const app=express();
+dotenv.config();
+
+
+app.get('/',(req,res)=>{
+    try {
+            console.log("I am talking from server 14")
+
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error",error:error})
+        
+    }
+})
+
+const port=process.env.PORT13 || 4343;
+dbConnect().then(()=>{
+    try {
+        app.listen(port,()=>{
+            console.log(`Server is running at http://localhost:${port}`)
+        })
+
+        
+    } catch (error) {
+        console.error("Database connection failed:",error)
+    }
+})
+
