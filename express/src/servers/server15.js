a06ed87c6b27b8d8ba74396c1746df6c4f7bab9c
Basit Ali <opoali911@gmail.com>
2025-03-27 07:39:07 +0500

    create the end point of the register and login

--- express/src/servers/server15.js
+++ express/src/servers/server15.js
@@ -9,8 +9,7 @@ import dbConnect from '../config/db/dbConnection6';
 const app=express();
 dotenv.config();
 app.use(express())
-
-
+const user=[]
 
 
 app.get('/',(req,res)=>{
@@ -19,6 +18,43 @@ app.get('/',(req,res)=>{
         
     } catch (error) {
         console.error("Home page error:",error)
+        
+    }
+})
+
+app.post('/register',async(req,res)=>{
+    try {
+        const {userName,password}=req.body;
+        const hashPasword=await bcrypt.hash(password,10);
+        user.push({
+            userName,
+            password:hashPasword
+        })
+        res.status(200).json({message:"User register successfully.."})
+
+        
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"})
+        
+    }
+})
+
+app.post('/login',async(req,res)=>{
+    try {
+        const {userName,password}=req.body;
+    const users=user.find((user)=>user.userName===userName)
+    if(!users || !(await bcrypt.compare(password,users.password))){
+        return res.status(401).json({message:"Not Authorized"})
+    }
+    //? if the user is avaiable then we generate token by uisng jwt
+
+    const token=jwt.sign({userName:userName},process.env.JWT_SECRET_KEY)// this sign method required two parameters one is object where we assign data and the second parameter is JWT_SECRET_KEY
+
+
+    res.status(200).json(token) 
+        
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error",error})
         
     }
 })
