8d265b2921023c3a732eb5878918b4ff46b5d784
Basit Ali <opoali911@gmail.com>
2025-03-28 07:07:40 +0500

    almost finished the server code

--- express/src/servers/server16.js
+++ express/src/servers/server16.js
@@ -9,7 +9,10 @@ const app=express();
 dotenv.config();
 
 
-
+const port=process.env.PORT17 || 3433
 dbConnect().then(()=>{
-    
+    app.listen(port,()=>{
+        console.log(`Server is running at http://localhost:${port}`)
+    })
+
 })
