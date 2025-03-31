f4c290bfa5728e37a15d09ca710f03096bdb9775
Basit Ali <opoali911@gmail.com>
2025-03-31 14:30:19 +0500

    explore many information about function in python

--- Python/02_Python_FUN/Program1.py
+++ Python/02_Python_FUN/Program1.py
@@ -10,4 +10,17 @@ def square(number):
     return number*number;
 
 sq=square(2)
-print("Square of a number is:",sq)
+# print("Square of a number is:",sq)
+
+
+#function-3
+
+def loop(number):
+    for num in range(100):
+        if(num==number):
+            return "Good luck" 
+        else:
+            print( num)        
+
+loop=loop(80)
+# print("new function :",loop)
