528878d12b8b90363a4908d570d4045f0a2c0946
Basit Ali <opoali911@gmail.com>
2025-04-02 09:47:18 +0500

    explore the init function which is the contructor  and self key word in class

--- Python/03_OOP/program.py
+++ Python/03_OOP/program.py
@@ -2,6 +2,16 @@ class Car:
     brand=None
     model:2020
 
-my_car=Car()
+my_car= Car()
 my_car.brand="Apo"
-print(my_car.brands)
+print(my_car.brand)
+
+
+class Room:
+    def __init__(self,windowSize,DoorLength):
+        self.door=DoorLength
+        self.wind=windowSize
+
+my_room=Room(12,34)
+door=my_room.door
+print(door)
