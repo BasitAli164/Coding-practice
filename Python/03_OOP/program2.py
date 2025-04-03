7f88129ed8cb3999d7b1ab1c2f790c8536558c13
Basit Ali <opoali911@gmail.com>
2025-04-03 10:41:57 +0500

    explore the inheritance concets

--- Python/03_OOP/program2.py
+++ Python/03_OOP/program2.py
@@ -1,2 +1,13 @@
 class Car:
-    def __init__(selef):
+    def __init__(self,brand,model):
+        self.brand=brand
+        self.model=model
+
+class ElectricCar(Car):
+    def __init__(self,brand,model, batterySize):
+        super().__init__(brand,model)
+        self.bSize=batterySize
+    def display(self):
+        print("Car Brand:",self.brand,"\n","Car Model:",self.model,"\n","Car Battery Size:",self.bSize)
+
+car1=ElectricCar("Royal","R","100kwh")
