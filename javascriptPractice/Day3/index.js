622029f756c43d7ad6297b245609ee808d0982d4
Basit Ali <opoali911@gmail.com>
2025-04-04 12:05:20 +0500

    practicing interview question

--- javascriptPractice/Day3/index.js
+++ javascriptPractice/Day3/index.js
@@ -4,6 +4,27 @@ console.log(1-"1")
 console.log(1/"1")
 
 
-a=10
-console.log(a)
-var a;
+// a=10
+// console.log(a)
+// var a;
+// a=2
+// var a;
+// let a;// it generate error
+
+const str="pakistan"
+
+const arr=['a','b',18,'d',100,10]
+
+const num=arr.filter((n)=>{
+    console.log(typeof n)
+    if(typeof(n)==="number"){
+        return n 
+    }
+})
+const string=arr.filter((str)=>{
+    if(typeof(str)==="string"){
+        return str
+    }
+})
+console.log("Number of array is:",num)
+console.log("String of array is:",string)
