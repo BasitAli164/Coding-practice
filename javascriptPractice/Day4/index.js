20cee63156084111a422294f49fafb67af1cfaa3
Basit Ali <opoali911@gmail.com>
2025-04-07 13:35:59 +0500

    solve problems

---
+++ javascriptPractice/Day4/index.js
@@ -0,0 +1,17 @@
+let str="basit"
+let vo="aieouAIEOU"
+let final=[]
+function vowel(str){
+    let arr=str.split("")
+    // console.log(arr)
+    for(i=0;i<=arr.length;i++){
+        console.log("i:",arr[i])
+        if(arr[i]==vo[i]){
+            
+
+        }
+    }
+
+}
+
+vowel(str)
