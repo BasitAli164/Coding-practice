3f1c3788c12044584e3b82a20df5f224e8896cc3
Basit Ali <opoali911@gmail.com>
2025-06-25 20:19:55 +0500

    create a method inside the class

--- Python/oop/program.py
+++ Python/oop/program.py
@@ -2,8 +2,12 @@ class House:
     def __init__(self,name,color):
         self.name=name
         self.color=color
+    def display(self):
+        print(f"Name:{self.name}\nColor:{self.color}")
+
 
 myHouse=House("Ali's House","white")
+myHouse.display()
 
-print(f"House Name: {myHouse.name}\nHouse Color: {myHouse.color}")
+# print(f"House Name: {myHouse.name}\nHouse Color: {myHouse.color}")
         
