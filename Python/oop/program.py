58914a67b2292307fd48285f8620c0511aef05d2
Basit Ali <opoali911@gmail.com>
2025-06-25 20:17:16 +0500

    create a class houes and an object and call the attributes by using dot operatot

---
+++ Python/oop/program.py
@@ -0,0 +1,9 @@
+class House:
+    def __init__(self,name,color):
+        self.name=name
+        self.color=color
+
+myHouse=House("Ali's House","white")
+
+print(f"House Name: {myHouse.name}\nHouse Color: {myHouse.color}")
+        
