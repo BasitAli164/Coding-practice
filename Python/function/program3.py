5874ccbbc8e48ede287c008e0d4de62657b5ff82
Basit Ali <opoali911@gmail.com>
2025-06-27 17:41:44 +0500

    now explore the higher order function in python

--- Python/function/program3.py
+++ Python/function/program3.py
@@ -3,6 +3,8 @@
 Higher Order Function:
     It is a type of function which take another function as a parameter/input and return a function as an output
     The following are the higher order function 
+    -map()
+    -filter()
 '''
 
 
