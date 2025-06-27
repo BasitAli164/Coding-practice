4da53146670f94f39e5706842db6c88a3191ac90
Basit Ali <opoali911@gmail.com>
2025-06-27 17:40:29 +0500

    learn about the higher order function

--- Python/function/program3.py
+++ Python/function/program3.py
@@ -1,7 +1,8 @@
 # Higher Function 
 '''
 Higher Function:
-    It is a type of function which take another function as a parameter
+    It is a type of function which take another function as a parameter/input and return a function as an output
+    The following are the higher order function 
 '''
 
 
