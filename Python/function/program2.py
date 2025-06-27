49cac560287b9b0dbe91374edd056940da9d4f36
Basit Ali <opoali911@gmail.com>
2025-06-27 16:59:18 +0500

    create a user define function which takes tow parameter and one is set by default value and call that and successfully execute that

--- Python/function/program2.py
+++ Python/function/program2.py
@@ -1,11 +1,20 @@
-# User Define function 
-def showMessage():
-    ''' simply show the message'''
-    print("Hi! I am a function ")
+# # User Define function 
+# def showMessage():
+#     ''' simply show the message'''
+#     print("Hi! I am a function ")
 
-result=showMessage()
-print("Id of result 1:",id(result))
-def showMessages(message):
-    print(message)
-results=showMessages("Hi! How are you")
-print("Id of results 2:",id(results))
+# result=showMessage()
+# # print("Id of result 1:",id(result))
+# def showMessages(message):
+#     print(message)
+# results=showMessages("Hi! How are you")
+# # print("Id of results 2:",id(results))
+
+
+
+def sum(a,b=9):
+    return a+b
+add=sum(4)
+print("Sum of two number is:",add)
+add=sum(4,4)
+print("Sum of two number is:",add)
