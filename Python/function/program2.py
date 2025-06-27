4475063b8053dc612e5eb5640c92c8cd10fb4b2f
Basit Ali <opoali911@gmail.com>
2025-06-27 16:55:13 +0500

    totally confused

--- Python/function/program2.py
+++ Python/function/program2.py
@@ -1,4 +1,11 @@
 # User Define function 
-
 def showMessage():
+    ''' simply show the message'''
     print("Hi! I am a function ")
+
+result=showMessage()
+print("Id of result 1:",id(result))
+def showMessages(message):
+    print(message)
+results=showMessages("Hi! How are you")
+print("Id of results 2:",id(results))
