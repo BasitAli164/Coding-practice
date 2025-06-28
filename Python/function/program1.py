c42206d8ac01d380b493ddfc5cb10f18f5c180d9
Basit Ali <opoali911@gmail.com>
2025-06-28 16:34:18 +0500

    exploring built function

--- Python/function/program1.py
+++ Python/function/program1.py
@@ -7,7 +7,20 @@ There are two types of function exits in python
 
 #Built-function
 
+
+# print and input
 print("Hello world")
 userName=input("Enter your name: ")
 print("My name is:",userName)
 print("Type of userName:",type (userName))
+
+
+# sorted
+price=[34,12,87,42,12,3]
+print("Sorted Price is:",sorted(price))
+
+#len
+print("Length of userName:",len(userName))
+print("Length of Price:",len(price))
+
+
