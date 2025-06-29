c49a7f14d072ea529b7e5aab9e8c645303d51d1a
Basit Ali <opoali911@gmail.com>
2025-06-29 10:41:16 +0500

    explore conditional statement

--- Python/conditionalStatements/program1.py
+++ Python/conditionalStatements/program1.py
@@ -4,3 +4,13 @@ if marks>60:
     print("Greate")
 else:
     print("well")
+
+
+
+age=int(input("Enter your age: "))
+if age>70 and age<100:
+    print("You are an old man")
+elif age>30 and age<70:
+    print("You are a young man")
+else:
+    print("You are a child")
