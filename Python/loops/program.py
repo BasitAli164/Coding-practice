ce9577c2041949947ba57d0c34b6b750362735ef
Basit Ali <opoali911@gmail.com>
2025-06-30 09:32:28 +0500

    create a program on loop

---
+++ Python/loops/program.py
@@ -0,0 +1,11 @@
+'''
+-->Loop: indicate for itration or repeatation 
+-->Types:
+        The following are the type of loops in python:
+        i- for loop
+        ii- while loop
+
+'''
+
+for item in "Basit":
+    print(item)
