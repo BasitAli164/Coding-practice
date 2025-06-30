8b99b1eca60e6b1b5ecb231dd825296b8bbe7937
Basit Ali <opoali911@gmail.com>
2025-06-30 10:12:27 +0500

    now learn another loop which is while

--- Python/loops/program1.py
+++ Python/loops/program1.py
@@ -1,10 +1,11 @@
+#List
 list1=[2,3,4,56,["basit",3,True,[(1,2,3,4,5),{"Name":"basit","age":23}]]]
 for item in list1:
     print(item)
 list1=[2,3,4,56,["basit",3,True,[(1,2,3,4,5),{"Name":"basit","age":273}]]]
 for item in list1[4]:
     print(item)
-
+#Dictionary
 dict={
     "name":"Basit Ali",
     "age":23,
@@ -25,3 +26,10 @@ car={
 
 for item in car:
     print(f"{car[item]}")
+
+# now we can access the key and value from dictionary through the built-in function item()
+for i, j in car.items():
+    print(f"Key:{i}----Value:{j}")
+
+
+
