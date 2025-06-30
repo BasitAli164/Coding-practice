fa020b86356c5f12480826bdb1c4539c43b52a94
Basit Ali <opoali911@gmail.com>
2025-06-30 09:41:15 +0500

    exploring  new concept in loop

--- Python/loops/program.py
+++ Python/loops/program.py
@@ -6,13 +6,22 @@
         ii- while loop
 
 '''
-
+print("Simple Loop Program")
 for item in "Basit":
-    print(item," value:")
+    print("Value:",item)
 
  # enumerate return index and value both without using enumerate we write more code
+print("\nUsing Enumerate")
 for i,j in enumerate("King"):
     print(f"Index- {i}:{j}")
 
 
-#
+#Without enumerate
+
+myName="Basit Ali"
+index=0
+
+print("\nWithout Enumerate")
+for item in myName:
+    print(f"Index: {index} and Value: {item}")
+    index+=1
