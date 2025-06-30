e4ad56c66f20b84e2474b19cca45a7de138c6531
Basit Ali <opoali911@gmail.com>
2025-06-30 09:57:45 +0500

    now start to explore loop with dictionary

--- Python/loops/program1.py
+++ Python/loops/program1.py
@@ -1,3 +1,15 @@
 list1=[2,3,4,56,["basit",3,True,[(1,2,3,4,5),{"Name":"basit","age":23}]]]
 for item in list1[4]:
     print(item)
+
+
+dict={
+    "name":"Basit Ali",
+    "age":23,
+    "gender":"male",
+    "Student":True
+}
+
+
+for key in dict:
+    print(f"{key} and {dict[key]}")
