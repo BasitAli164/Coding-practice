6f679348cf7775fc50205a9958d397a4eb6712dd
Basit Ali <opoali911@gmail.com>
2025-07-10 10:55:26 +0500

    working in inheritance

--- Python/03_OOP/program3.py
+++ Python/03_OOP/program3.py
@@ -8,5 +8,20 @@ class Student:
 
 
 std1=Student("Basit","S23BSCS012")
-print(std1.display())
+# print(std1.display())
 
+
+class Father:
+    def __init__(self,name,address):
+        self.fname=name
+        self.faddr=address
+
+class Son(Father):
+    def __init__(self,fname,faddr,sname):
+        super().__init__(fname,faddr)
+        self.sname=sname
+    def display(self):
+        return f"Father Name:{self.fname} \nFather Address:{self.faddr} \nSon Name:{self.sname}"
+
+s1=Son("Ali","Kushmarah","Basit Ali")
+print(s1.display())
