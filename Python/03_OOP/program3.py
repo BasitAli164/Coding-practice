c934a6e98f4a06a3228519827b77aae58e75f13e
Basit Ali <opoali911@gmail.com>
2025-07-10 10:42:35 +0500

    expore the formating string like litral in js

---
+++ Python/03_OOP/program3.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name,reg):
+        self.name=name
+        self.reg=reg
+    def display(self):
+        return f"Name: {self.name} Reg# :{self.reg}"
+    
+
+
