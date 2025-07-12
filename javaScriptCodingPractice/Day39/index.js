7f30869abfa98951fdd2d914f4461139e6536ce3
Basit Ali <opoali911@gmail.com>
2025-07-13 00:20:59 +0500

    exploring new basic concept of js

--- javaScriptCodingPractice/Day39/index.js
+++ javaScriptCodingPractice/Day39/index.js
@@ -1,17 +1,22 @@
-//! Question-1
-// console.log(typeof typeof typeof  100)
+// //! Question-1
+// // console.log(typeof typeof typeof  100)
 
-//! Question-2
-let str="basit is my name ";
-let split=str.split("").reverse().join("");
-// console.log(split)
+// //! Question-2
+// let str="basit is my name ";
+// let split=str.split("").reverse().join("");
+// // console.log(split)
 
-//! Question-3
-    function abc(){
-        console.log(abc)
-    }
-    abc()
-    abc.xyz=400;
-    abc.xyz=200;
-    console.log(abc())
-    abc()
+// //! Question-3
+//     function abc(){
+//         console.log(abc)
+//     }
+//     abc()
+//     abc.xyz=400;
+//     abc.xyz=200;
+//     console.log(abc())
+//     abc()
+
+//! Question-4
+const arr=[1,2,3,4]
+arr[100]=500;
+console.log(arr)
