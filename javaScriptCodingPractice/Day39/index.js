d0a5bd2ef5bc4ac37d3d87323b582a567c471fff
Basit Ali <opoali911@gmail.com>
2025-07-12 23:49:48 +0500

    solving question

--- javaScriptCodingPractice/Day39/index.js
+++ javaScriptCodingPractice/Day39/index.js
@@ -1,1 +1,9 @@
-console.log(typeof typeof typeof  100)
+//! Question-1
+// console.log(typeof typeof typeof  100)
+
+//! Question-2
+let str="basit is my name ";
+let split=str.split("").reverse().join("");
+// console.log(split)
+
+//! Question-3
