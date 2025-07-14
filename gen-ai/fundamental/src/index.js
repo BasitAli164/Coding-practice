a71a8e50e9e5f8ea05ffd37eb7ecd09376165c4f
Basit Ali <opoali911@gmail.com>
2025-07-14 23:05:40 +0500

    fix the error which i facing in gen-ai project

--- gen-ai/fundamental/src/index.js
+++ gen-ai/fundamental/src/index.js
@@ -1,1 +1,10 @@
 import {OpenAI} from 'openai';
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+// create the instance of the openai 
+const openai=new OpenAI({
+    apiKey:process.env.OPEN_AI_KEY
+})
+
