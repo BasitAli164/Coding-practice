aea06ebafdc7329ff96e4d3224d030e8811c8542
Basit Ali <opoali911@gmail.com>
2025-07-14 23:34:58 +0500

    learn how to generate a paragraph according to our prmpt

--- gen-ai/fundamental/src/index.js
+++ gen-ai/fundamental/src/index.js
@@ -7,12 +7,32 @@ const openai = new OpenAI({
   apiKey:process.env.OPEN_AI_KEY,
 });
 
-const completion = openai.chat.completions.create({
-  model: "gpt-4o-mini",
-  store: true,
-  messages: [
-    {"role": "user", "content": "write a paragraph on pakistan"},
-  ],
-});
+// const completion = openai.chat.completions.create({
+//   model: "gpt-4o-mini",
+//   store: true,
+//   messages: [
+//     {"role": "user", "content": "write a paragraph on pakistan"},
+//   ],
+// });
 
-completion.then((result) => console.log(result.choices[0].message));
+// completion.then((result) => console.log(result.choices[0].message));
+
+
+const main =async()=>{
+    const prompt="Write a paragraph on Gilgit baltistan";    
+    const completion=await openai.chat.completions.create({
+        model:"gpt-4o-mini",
+        messages:[
+            {
+                role:"system",
+                content:"Your are a personal trainer"
+            },
+            {
+                role:"user",
+                content:prompt
+            }
+        ]
+    })
+    console.log(completion.choices[0].message.content)
+}
+main()
