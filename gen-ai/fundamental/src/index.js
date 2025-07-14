6bf830630a5f18289dece29dc0f00deed2cd50a4
Basit Ali <opoali911@gmail.com>
2025-07-14 23:20:31 +0500

    create my first gen ai project where i use gpt 40 mini model

--- gen-ai/fundamental/src/index.js
+++ gen-ai/fundamental/src/index.js
@@ -1,10 +1,18 @@
-import {OpenAI} from 'openai';
-import dotenv from 'dotenv'
+import OpenAI from "openai";
+import dotenv from "dotenv"
+
 dotenv.config()
 
+const openai = new OpenAI({
+  apiKey:process.env.OPEN_AI_KEY,
+});
 
-// create the instance of the openai 
-const openai=new OpenAI({
-    apiKey:process.env.OPEN_AI_KEY
-})
+const completion = openai.chat.completions.create({
+  model: "gpt-4o-mini",
+  store: true,
+  messages: [
+    {"role": "user", "content": "write a paragraph on pakistan"},
+  ],
+});
 
+completion.then((result) => console.log(result.choices[0].message));
