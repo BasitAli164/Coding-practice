5335f61af3782bc4ddf7c28e6b3d3a9a65da8ea4
Basit Ali <opoali911@gmail.com>
2025-07-19 18:43:50 +0500

    now working with json which stand for javascript object notation and explore it techniques

---
+++ Python/JSON/program.py
@@ -0,0 +1,12 @@
+'''
+JSON: Javscript Object Notation
+    --> it is a method to store and exchnage of data , for any language when you working with api and store and update or modify data between different file and locatoin 
+
+    --> it is like to dictionary in python
+    --> Python give a built-in function 
+'''
+
+
+import json
+
+data={"name":"basit","age":"12","city":"skardu"}
