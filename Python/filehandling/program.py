384ac025c8c998bf7611a110b2cdf8a05dcffe0f
Basit Ali <opoali911@gmail.com>
2025-07-19 18:17:54 +0500

    write a simple and minimilist note of filehandling in python

--- Python/filehandling/program.py
+++ Python/filehandling/program.py
@@ -1,4 +1,17 @@
-# create a file
- # here one is you file name with its extention and another one is mode which is used to take which type of operation you perform like reading file, write somthing in file , delete the file 
+''' create a file
+ here one is you file name with its extention and another one is mode which is used to take which type of operation you perform like reading file, write somthing in file , delete the file 
 # syntax:
-file=open("text.txt","mode")
+1 .variable=open("file name","mode")
+2. variable.permissionModeMethod("wirte something")
+3. varibale.close()
+    example:
+    file=open("text.txt","w")----> here text.txt is a file and w is a permission of writing 
+    file.write("Hello i am basit ") --> after open file and give the permit of writting we write anything through write method 
+    file.close() ---> after open a file it is mendatory to close that file using close method
+
+'''
+
+
+
+
+
