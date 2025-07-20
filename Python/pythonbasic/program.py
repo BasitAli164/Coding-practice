a67bf7d78a26594b940f94e89bd1c67f4b1b2468
Basit Ali <opoali911@gmail.com>
2025-07-20 09:06:37 +0500

    now explore the variables and data type

--- Python/pythonbasic/program.py
+++ Python/pythonbasic/program.py
@@ -1,1 +1,8 @@
+'''
+1. Write First Program in python
+2. Variable: It is container that hold the value which assign 
+3.
+
+'''
 print("Hello World") # print is a built-in function which provide python by default
+
