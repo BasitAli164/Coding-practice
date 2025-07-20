27a7be2d845a2f5222f2f648e1d0a3a2b384ef11
Basit Ali <opoali911@gmail.com>
2025-07-20 09:20:30 +0500

    exploring variables and datatype in python

--- Python/pythonbasic/program.py
+++ Python/pythonbasic/program.py
@@ -3,11 +3,23 @@
 2. Variable: It is container that hold the value which assign 
     syntax: varibaleName, assignment operator and Value ----> userName="Basit Ali"
 3. Rules and Regulation to create a varibble ---> only should be  create variable by using underscore(_)and letter
+4.Datatypes in python
 
 '''
+
+# Print fucniton 
 print("Hello World") # print is a built-in function which provide python by default
 
+
+# Variables
 userName="Apo" # by using camelCase naming formate i create a variable
 first_name="Basit" # by using snakeCase naming formate i create a variable 
 LastName="Ali" # by using pascal case naming format i create a varibale
+
+# Variable and datatypes
+
+myName="Basit Ali" # string
+myAge=23 # number(int)
+myWeight=70.23 # number(float)
+isReady=True # boolean
 
