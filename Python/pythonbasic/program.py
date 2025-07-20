67905994d47c949f9a3314b0651ac637d10c453a
Basit Ali <opoali911@gmail.com>
2025-07-20 09:09:09 +0500

    exploring how to keep variable name

--- Python/pythonbasic/program.py
+++ Python/pythonbasic/program.py
@@ -1,8 +1,11 @@
 '''
 1. Write First Program in python
 2. Variable: It is container that hold the value which assign 
-3.
+    syntax: varibaleName=Value
 
 '''
 print("Hello World") # print is a built-in function which provide python by default
+
+userName="Apo" # by using pascal naming formate i create a variable
+first_name="Basit" # by using snake naming formate i create a variable 
 
