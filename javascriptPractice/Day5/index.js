fd399874c38cb0dfecf9921640c1c529ad92843b
Basit Ali <opoali911@gmail.com>
2025-07-29 08:48:30 +0500

    practice one question in array

---
+++ javascriptPractice/Day5/index.js
@@ -0,0 +1,6 @@
+let days=["Sunday","Wednesday","Tuesday","Friday"]
+
+let index=days.findIndex((day)=>day==="monday")
+let index1=days.findIndex((day)=>day==="sunday")
+
+console.log("Index: ",index, "\n","Index 1: ",index1)
