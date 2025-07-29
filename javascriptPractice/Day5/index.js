defb4ef81df9ff0f0a267bffa078367d7e2eff10
Basit Ali <opoali911@gmail.com>
2025-07-29 08:56:33 +0500

    explore new thing reated to array method and practce them with example

--- javascriptPractice/Day5/index.js
+++ javascriptPractice/Day5/index.js
@@ -3,4 +3,13 @@ let days=["Sunday","Wednesday","Tuesday","Friday"]
 let index=days.findIndex((day)=>day==="monday")
 let index1=days.findIndex((day)=>day==="sunday")
 
-console.log("Index: ",index, "\n","Index 1: ",index1)
+// console.log("Index: ",index, "\n","Index 1: ",index1)
+
+
+let number=[1,2,3,4,5,6,7]
+
+const include=number.includes(2)
+// console.log("Include: ",include)
+
+number.splice(1,4,3)
+console.log("Array : ",number)
