dbada961a90f9e66265ddebe8ef24eb13f8a1b14
Basit Ali <opoali911@gmail.com>
2025-08-04 09:07:25 +0500

    explore the formating in pandas

--- pandas/pandasbasic/main.py
+++ pandas/pandasbasic/main.py
@@ -1,7 +1,14 @@
 # import pandas and use alias name pd for pandas
 import pandas as pd;
 
+# Read file from local with pandas method and store in a variable
 
+'''
+In pandas there are two type of expressing format of data which are 
+    i-series: one dimentional array ---> single row
+    ii- Datafram : two dimentional array ---> combination of row and column means table
+
+'''
 df=pd.read_csv("car_prices.csv",encoding="utf-8")
 
 print(df)
