2ca7835df9ad95decc8e7ec2b29c792f1b2a7eba
Basit Ali <opoali911@gmail.com>
2025-08-06 07:56:59 +0500

    explore tow methods in pandas which are head and tail these method  are fetch the values from dataset from top and bottom

--- pandas/pandasbasic1/again.py
+++ pandas/pandasbasic1/again.py
@@ -1,8 +1,12 @@
 import pandas as pd;
 
 df=pd.read_csv("car_prices.csv")
-print(df)
-
+# print(df)
+print(df.shape)
+print(df.head()
+)
+print("Tail: ")
+print(df.tail(2))
 
 person={
     "name":["Basit"],
@@ -13,3 +17,4 @@ person={
 df1=pd.DataFrame(person)
 
 df1.to_csv("output.csv",index=False)
+print(df1.shape)
