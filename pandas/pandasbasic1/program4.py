3c2dbf194ae9018f30ff915075f22177ffc13534
Basit Ali <opoali911@gmail.com>
2025-08-06 08:14:28 +0500

    now creating notes

--- pandas/pandasbasic1/program4.py
+++ pandas/pandasbasic1/program4.py
@@ -14,5 +14,5 @@ df1=pd.DataFrame(listOfPerson)
 df1.to_json("jsonfile.json")
 
 
-print("Top data:\n ",df.head())
+print("Top data:\n ",df.head()) # in tail and head method of pandas we can pass an integer value if we don't pass then by default it set by 5
 print("Bottom data:\n ",df.tail())
