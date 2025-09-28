8057c287fb1a736542abd3d4f29199d7a0e0a783
Basit Ali <opoali911@gmail.com>
2025-09-28 09:34:49 +0500

    solving problems

--- javascriptPractice/Day6/index.js
+++ javascriptPractice/Day6/index.js
@@ -1,20 +1,39 @@
-const arr=["basit","ing"];
+// const arr=["basit","ing"];
 
-function checkSubString(arr) {
-    let arr1=arr[0].split("")
-    let arr2=arr[1].split("")
-    let flag=false;
+// function checkSubString(arr) {
+//     let arr1=arr[0].split("")
+//     let arr2=arr[1].split("")
+//     let flag=false;
 
-    for(let i=0;i<arr2.length;i++){
-        if(arr1.includes(arr2[i])){
-            flag=true
-        }else{
-            flag=false
-            console.log("false is done")
-        }
-    }
-    return flag
+//     for(let i=0;i<arr2.length;i++){
+//         if(arr1.includes(arr2[i])){
+//             flag=true
+//         }else{
+//             flag=false
+//             console.log("false is done")
+//         }
+//     }
+//     return flag
     
-}
+// }
 
-console.log(checkSubString(arr))
+// console.log(checkSubString(arr))
+
+
+// let obj=[
+//     {name:"basit",age:23},
+//     {name:"muhammad",age:30},
+//     {name:"king",age:19},
+// ]
+
+
+// age=obj.filter((obj)=>obj.age<24)
+
+// console.log(age)
+
+
+// function abc(a,...b){
+//     console.log(b,typeof b)
+// }
+
+// abc(10,8,7)
