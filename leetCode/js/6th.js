90acf4293a6bd8203f67968449124b4101a66830
Basit Ali <opoali911@gmail.com>
2025-12-05 14:02:08 +0500

    start to perform basic problems

--- leetCode/js/6th.js
+++ leetCode/js/6th.js
@@ -1,14 +1,38 @@
-function squareOfArray(arr){
-    let newArr=[]
-    for(let i=0;i<arr.length;i++){
-        newArr=arr[i*i]
-        console.log(newArr)
+// function squareOfArray(arr){
+//     let newArr=[]
+//     for(let i=0;i<arr.length;i++){
+//         newArr=arr[i*i]
+//         console.log(newArr)
+
+//     }
+//     return newArr
+// }
+// console.log(squareOfArray([2,3,4,5]))
+
+//! Power or any number
+
+// let num=2,pow=3,result=num;
+// for (let i=1;i<pow;i++){
+//     result=result*num
+    
+//     console.log("Result is: ",result)
+// }
+// console.log("Result is: ",result)
+
+
+//! Prime number
+
+let n=3;
+if(n<2){
+    console.log("Not Prime")
+}else{
+    for(let i=2;i<n;i++){
+        if(n%i==0){
+            console.log("Composite")
+            return 0
+        }
 
     }
-    return newArr
+    console.log("prime")
+    return 0
 }
-console.log(squareOfArray([2,3,4,5]))
-
-
-
-
