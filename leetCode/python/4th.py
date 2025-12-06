3652d18fe87471031ce03efefe162eb75cfe4df9
Basit Ali <opoali911@gmail.com>
2025-12-06 20:00:49 +0500

    fix the issue

--- leetCode/python/4th.py
+++ leetCode/python/4th.py
@@ -1,9 +1,10 @@
-num=98,ans=0
+num=98
+ans=0
 while(num>9):
     
     while(num!=0):
         rem=num%10
-        num/=10
+        num=num//10
         ans=ans+rem
     num=ans
 print("Number is:",num)
