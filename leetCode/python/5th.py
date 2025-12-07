ac44cbc2d2165cd546ea01641d4037aac13a9fda
Basit Ali <opoali911@gmail.com>
2025-12-07 10:24:56 +0500

    done more one question

--- leetCode/python/5th.py
+++ leetCode/python/5th.py
@@ -6,6 +6,19 @@ class ReverseDigit:
             num//=10
             ans=ans*10+rem
         print("Number is:",ans)
+    def powerOfTwo(self,num:int)->int:
+        if(num<1):
+            print("Not comes in power of two")
+            return 0
+        while(num>1):
+            if num%2==1:
+                print("Not comes in power of two")
+                return 0
+            num=num//2
+        print("Comes in power of two")
+        return 1
 
 obj1=ReverseDigit()
 obj1.reversedigits(457)
+obj1.powerOfTwo(23)
+obj1.powerOfTwo(32)
