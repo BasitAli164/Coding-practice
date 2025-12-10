1a627bc2a41f6c7d073d6b3c5607efa62c5ff791
Basit Ali <opoali911@gmail.com>
2025-12-10 16:43:25 +0500

    solve the problem

--- leetCode/js/11th.js
+++ leetCode/js/11th.js
@@ -16,10 +16,17 @@ console.log("Tow sum is: ", towSum([1, 6, 3, 12, 8], 20));
 
 function duplicateValue(arr){
     let left=0,right=1,count=1;
-    while(left<right){
+    while(right<arr.length){
         if(arr[left]!=arr[right]){
-            
+            arr[left+1]=arr[right]
+            count++
+            left++
+            right++
+        }else{
+            right++
         }
     }
+    console.log("Unique Ele exit in array:",count)
 }
+duplicateValue([1,1,1,1,1,2,2,2,3,3,4,4,5])
 
