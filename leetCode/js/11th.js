8af767d48c5983383f54a151897b33232d0116ad
Basit Ali <opoali911@gmail.com>
2025-12-11 10:49:52 +0500

    facing error

--- leetCode/js/11th.js
+++ leetCode/js/11th.js
@@ -1,34 +1,48 @@
 function towSum(arr, target) {
-    // arr.sort((a,b)=>a-b) // if sort is required
-    console.log("array is: ",arr)
+  // arr.sort((a,b)=>a-b) // if sort is required
+  console.log("array is: ", arr);
   let left = 0,
     right = arr.length - 1;
   while (left < right) {
     if (arr[left] + arr[right] == target) return [arr[left], arr[right]];
     else if (arr[left] + arr[right] > target) right--;
     else if (arr[left] + arr[right] < target) left++;
-    
   }
   return 0;
 }
-console.log("Tow sum is: ", towSum([1, 6, 3, 12, 8], 20));
+// console.log("Tow sum is: ", towSum([1, 6, 3, 12, 8], 20));
 
-
-function duplicateValue(arr){
-    let left=0,right=1,count=1;
-    while(right<arr.length){
-        if(arr[left]!=arr[right]){
-            arr[left+1]=arr[right]
-            count++
-            left++
-            right++
-        }else{
-            right++
-        }
+function duplicateValue(arr) {
+  let left = 0,
+    right = 1,
+    count = 1;
+  while (right < arr.length) {
+    if (arr[left] != arr[right]) {
+      arr[left + 1] = arr[right];
+      count++;
+      left++;
+      right++;
+    } else {
+      right++;
     }
-    console.log("Unique Ele exit in array:",count)
+  }
+  console.log("Unique Ele exit in array:", count);
 }
 // duplicateValue([1,1,1,1,1,2,2,2,3,3,4,4,5])
 
-
-
+function binarySearch(arr, key) {
+  let left = 0,
+    right = arr.length;
+  while (left <= right) {
+    let mid = Math.floor(left + right / 2);
+    if (arr[mid] == key) {
+      return mid;
+    } else if (arr[mid] > key) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 6));
