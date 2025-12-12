61e9dbe50b28c0fff01f1b88cc09842a747d7bb7
Basit Ali <opoali911@gmail.com>
2025-12-12 10:45:57 +0500

    start to solve new problem

--- leetCode/python/6th.py
+++ leetCode/python/6th.py
@@ -15,17 +15,20 @@
 # print(result)
 
 
-def binarySearch(arr:list,key:int)->int:
-    left=0
-    right=len(arr)-1
-    while(left<=right):
-        mid=((left+right)//2)
-        if(arr[mid]==key):
-            return mid
-        elif arr[mid]>key:
-            right=mid-1
-        else:
-            left=mid+1
-    return -1
+# def binarySearch(arr:list,key:int)->int:
+#     left=0
+#     right=len(arr)-1
+#     while(left<=right):
+#         mid=((left+right)//2)
+#         if(arr[mid]==key):
+#             return mid
+#         elif arr[mid]>key:
+#             right=mid-1
+#         else:
+#             left=mid+1
+#     return -1
 
-print(binarySearch([1,2,3,4,5,6,7,8,9],6))
+# print(binarySearch([1,2,3,4,5,6,7,8,9],6))
+
+
+
