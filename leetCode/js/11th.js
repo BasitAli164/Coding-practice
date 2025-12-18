201911567f88fa5bcf94765a442892fcdd9ed17f
Basit Ali <opoali911@gmail.com>
2025-12-18 11:37:46 +0500

    solving the peak index in mountain problem

--- leetCode/js/11th.js
+++ leetCode/js/11th.js
@@ -82,29 +82,3 @@ function findTarget(arr,target){
 }
 // console.log(findTarget([5,7,7,8,8,9],8))
 
-function maxNumberIndex(arr){
-  let max=arr[0], index=0;
-  for(let i=1;i<=arr.length;i++){
-    if(arr[i]>max){
-      max=arr[i]
-      index=i
-    }
-  }
-  return index
-
-}
-function maxNumberIndex2(arr){
-  let max=arr[0],left=0,right=arr.length-1;
-  while(left<=right){
-    mid=Math.floor((left+right)/2)
-    if(arr[mid]>max){
-      max=arr[mid]
-      left=mid+1
-    }
-    else{
-      right=mid-1
-    }
-  }
-  return max
-}
-console.log("Max number index is: ",maxNumberIndex2([2,4,6,8,10,12,14,8,5,3]))
---
+++ leetCode/js/12th.js
@@ -0,0 +1,28 @@
+//!
+function maxNumberIndex(arr){
+  let max=arr[0], index=0;
+  for(let i=1;i<=arr.length;i++){
+    if(arr[i]>max){
+      max=arr[i]
+      index=i
+    }
+  }
+  return index
+
+}
+function maxNumberIndex2(arr){
+  let max=arr[0],left=0,right=arr.length-1;
+  while(left<=right){
+    mid=Math.floor((left+right)/2)
+    if(arr[mid]>max){
+      max=arr[mid]
+      left=mid+1
+    }
+    else{
+      right=mid-1
+    }
+  }
+  return max
+}
+console.log("Max number index is: ",maxNumberIndex([2,4,6,8,10,12,14,8,5,3])) // bruteforce  approach
+console.log("Max number index is: ",maxNumberIndex2([2,4,6,8,10,12,14,8,5,3]))// optimize approach
